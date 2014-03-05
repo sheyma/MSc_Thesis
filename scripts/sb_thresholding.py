50cde810d460e01fdda162c04192ca3b11cbe84e
Şeyma Bayrak <seymaba@gmail.com>
2014-03-05 18:38:50 +0100

    nodes and edges of graph

--- scripts/sb_thresholding.py
+++ scripts/sb_thresholding.py
@@ -1,47 +1,56 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-def get_threshold_matrix(filename, threshold_value):
-	import networkx as nx
-	import numpy as np
-	
-	A = np.transpose(np.loadtxt(filename, unpack=True))
+# sb_thresholding.py : creates a thresholded matrix and saves it
 
-	B = np.zeros(len(A),len(A))
-	
-	for row in range(len(A)):
-		for item in range(len(A)):
-			if row != item:
-				if A[row,item] >= threshold_value:
-					B[row,item] = 1
-				else:
-					B[row,item] = 0 
-	
-	G=nx.from_numpy_matrix(B,create_using=nx.Graph())
-	return G
+import networkx as nx
+import numpy as np 
+import sys  
 
-def print_adjacency_matrix(G):
-	import networkx as nx
-	print nx.adjacency_matrix(G)
+def get_threshold_matrix(input_mtx, r):
+  A = np.transpose(np.loadtxt(input_mtx, unpack=True)) 
+  B = np.zeros((len(A),len(A)))
+
+  for row in range(len(A)):
+    for item in range(len(A)):
+      if row != item:
+        if A[row,item] >= r:
+          B[row,item] = 1
+        else:
+          B[row,item] = 0
+
+  G=nx.from_numpy_matrix(B,create_using=nx.Graph())  #result of function is graph G
+  return G
 
 
-def export_adjacency_matrix(G, filename, threshold_value):
-	hiwi = nx.adjacency_matrix(G)
+def print_adjacency_matrix(G):			# use previous G here, print it
+  print nx.adjacency_matrix(G)
+
+
+def export_adjacency_matrix(G, input_mtx, r):		# save adjacency matrix
+
+  hiwi = nx.adjacency_matrix(G)
+  f = open(input_mtx[:-4]+'_r'+str(r)+'.dat','w')
+  for i in range(len(hiwi)):
+    for j in range(len(hiwi)):
+      f.write("%d\t" % (hiwi[i,j]))
+    f.write("\n")
+  f.close()
+
+
+
+if __name__ == '__main__':
   
-	f = open(filename[:-4]+'_r'+str(threshold_value)+'.dat','w')
-  	for i in range(len(hiwi)):
-    	for j in range(len(hiwi)):
-    	  f.write("%d\t" % (hiwi[i,j]))
-    	f.write("\n")
-  	f.close()
+  usage = 'Usage: %s correlation_matrix threshold' % sys.argv[0]
+  try:
+    infilename_data = sys.argv[1]
+    value = sys.argv[2]
+  except:
+    print usage; sys.exit(1)
 
+  ###manual choice of the threshold value
+  threshold = float(value)
+  network = get_threshold_matrix(infilename_data, threshold)
+  print_adjacency_matrix(network)
+  export_adjacency_matrix(network, infilename_data, threshold)
 
-
-	import sys
-	value = sys.argv[2]
-	infilename_data = sys.argv[1]
-
-	threshold = float(value)
-	network = get_threshold_matrix(infilename_data, threshold)
-	print_adjacency_matrix(network)
-	export_adjacency_matrix(network, infilename_data, threshold)
