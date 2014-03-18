d1f19464ca350b9e9add00dfa1dc3484abae2e16
Şeyma Bayrak <seymaba@gmail.com>
2014-03-18 12:55:02 +0100

    separate each network measurement into sub functions

--- scripts/sb_network_measures.py
+++ scripts/sb_network_measures.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-# sb_thresholding.py : creates a thresholded matrix and Graph G
-# in Graph G for many threshold values:
-# finds number of edges(links: L)
-# finds number of nodes, N
-# finds network density D = 2L/(N(N-1)) (kappa)
-# finds number of connected components
-# finds CC (clustering coefficient) of whole network
-# does check_sum and ave for degree distribution
-# finds average degree overall network (<kappa>)
-# find shortest pathway overall network
-# finds global efficieny of full network and each node
-# finds local efficiency of full network and each node
-# finds Small worldness of full network
-# finds clustering coefficients of each node
-# find connected component of each node
+# sb_thresholding.py : firsty binarize the input matrix (thresholding)
+# secondy creates the graph (network) of the binarized matrix
+# then measures the basic network properties
 
 import networkx as nx
 import numpy as np 
 import sys  
 from math import factorial
 
-def get_threshold_matrix(input_mtx, r):
+# create a thresholded matrix of test matrix and its graph
+def get_graph(input_mtx, r):
   A = np.transpose(np.loadtxt(input_mtx, unpack=True)) 
   B = np.zeros((len(A),len(A)))
 
@@ -36,51 +25,120 @@ def get_threshold_matrix(input_mtx, r):
 
   G=nx.from_numpy_matrix(B,create_using=nx.Graph()) 
   return G
-  #result of function graph G is to be used in all other functions
+  #result of function : graph G is to be used in all other functions
 
-def measures_of_network(input_mtx):
-  R = 0
-  f=open(input_mtx[:-4]+'_network_measures.dat','w')
-  #f.write('r(thres)\tL(Edges)\tD(densi.)\tcon_comp\tCC\tcheck_sum\tave_degr\n')
-  for i in range (0,101):
-	R = float(i)/100
-	G = get_threshold_matrix(input_mtx,R)
-	L = nx.number_of_edges(G) #total number of links : L
-	N = nx.number_of_nodes(G) #total number of nodes : N
-	max_edge = N*(N-1.)/2     # number of maximum edges(links)
-	Compon = nx.number_connected_components(G)
-	CC = nx.average_clustering(G)  # clustering coefficient of full network	
-	check_sum = 0.     		  # sum of degree distributions for one r
+# printing out the adjacency matrix of G
+def get_print_adjacency_matrix(G):
+	print nx.adjacency_matrix(G)
+
+# saving the adjacency matrix in as *.dat file
+def get_export_adjacency_matrix(G, filename, threshold):
+	M = nx.adjacency_matrix(G)
+	f = open(filename[:-4]+'r'+str(threshold)+'.dat', 'w')
+	for i in range(len(M)):
+		for j in range(len(M)):
+			f.write("%d\t" % ( M[i,j] ) )
+		f.write("\n")
+	f.close
+
+# a few characteristic measures of FULL network G with one threshold
+def get_characteristics(G,filename,R):
+
+	N = nx.number_of_nodes(G)		#total number of nodes : N
+	L = nx.number_of_edges(G)  		#total number of links : L
+	Compon = nx.number_connected_components(G) #number of connected components
+	cc = nx.average_clustering(G)	# clustering coefficient : cc
+	D = nx.density(G)				# network density: Kappa
+	check_sum = 0.
 	degree_hist = {}
-	values = []	
+	values = []
 	for node in G:
-		
-		if G.degree(node) not in degree_hist: # degree dist part
-			degree_hist[G.degree(node)] =1
+		if G.degree(node) not in degree_hist:
+			degree_hist[G.degree(node)] = 1
 		else:
-			degree_hist[G.degree(node)] +=1
-
-		values.append(G.degree(node))	# average degree part
+			degree_hist[G.degree(node)] += 1
+		values.append(G.degree(node))	
 	
-	ave=float(sum(values))/float(nx.number_of_nodes(G))	
-	# average degree overall network <k>
-	
+	ave_degree = float(sum(values)/float(N))	# average degree: <Kappa>
 	keys = degree_hist.keys()
-	keys.sort
-	for item in keys:		
-		check_sum +=float(degree_hist[item])/float(N)
+	keys.sort()
 	
-	f.write("%f\t%d\t%f\t%f\t%f\t%f\t%f\n" %(R,L,L/max_edge,Compon,CC,check_sum,ave))	
-	# 1.threshold, 2.L, 3.Density, 4.components, 5.CC_average, 6.check_sum, 7.sum_ave
-  f.close()
+	for item in keys :
+		check_sum += float(degree_hist[item])/float(N)
+	
+	print 'Test matrix: ', filename
+	print 'Threshold: ', R
+	print 'Number of nodes: ', N
+	print 'Number of links: ', L
+	print 'Number of connected components: ', Compon
+	print 'Clustering coefficient of full network: ', cc
+	print 'Check degree distribution sum: ', check_sum
+	print 'Network density: ', D
 
-def shortest_path(input_mtx):
+	print 'Average network degree: ', ave_degree 
+	return 0	
+
+# get L and D for full network for  different threshold values
+def get_number_of_edges_and_density(input_mtx):
+	R = 0
+	f = open(input_mtx[:-4]+'_edges_and_density.dat','w')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		L = nx.number_of_edges(G)
+		D = nx.density(G)
+		f.write("%f\t%d\t%f\n" % (R,L,D))
+		#1. threshold, 2. edges, 3. density
+	f.close()
+
+# get average clustering coefficient of full network for dif.thre.val.
+def get_average_cluster_coefficient(input_mtx):
+	R = 0
+	f = open(input_mtx[:-4]+'_cluster_coeffi_ave.dat','w')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		cc = nx.average_clustering(G)		
+		f.write("%f\t%f\n" % (R,cc))
+		#1. threshold, 2. clustering coefficient
+	f.close()
+
+# get average degree of full network for different threshold values
+def get_degrees_ave(input_mtx):
+	R = 0
+	f = open(input_mtx[:-4]+'_degree_ave.dat','w')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		N = nx.number_of_nodes(G) 
+		values = []
+		for node in G:
+			values.append(G.degree(node))
+		ave_degree = float(sum(values)) / float(N)			
+		f.write("%f\t%f\n" % (R,ave_degree))
+		#1. threshold, 2. average degree
+	f.close()
+
+# get number of connected components of full network for dif.thre.val.
+def get_connected_components(input_mtx):
+	R = 0
+	f = open(input_mtx[:-4]+'_connected_compo.dat','w')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		compon = nx.number_connected_components(G)
+		f.write("%f\t%f\n" % (R,compon))
+		#1. threshold, 2.number of connected components
+	f.close()
+
+# get shortest pathway of network
+def get_shortest_pathway(input_mtx):
 	R = 0
 	f = open(input_mtx[:-4]+'_shortest_path.dat','w')
 	#f.write('r(thre.)\tshorthest_pathlength\n')
 	for i in range(0,101):
 		R = float(i)/100
-		G = get_threshold_matrix(input_mtx,R)
+		G = get_graph(input_mtx,R)
 		Compon = nx.connected_component_subgraphs(G) # components
 		values_2 = []
 		for i in range(len(Compon)):
@@ -95,114 +153,15 @@ def shortest_path(input_mtx):
 			# 1.threshold , 2.shortest pathway
 	f.close()
 
-def degree_dist(input_mtx):			# degree distribution
+# get local efficiency for full network and single nodes separately
+def get_local_efficiency(input_mtx):
 	R = 0
-	f = open(input_mtx[:-4]+'_degree_dist.dat', 'w')
-	#f.write('node\tr(thre.)\tdeg_hist\tdeg_dist\n')
-	for i in range(0,101):
-		R = float(i)/100
-		G = get_threshold_matrix(input_mtx,R)
-		check_sum = 0.
-		degree_hist = {}
-		for node in G:
-			if G.degree(node) not in degree_hist:	
-				degree_hist[G.degree(node)] = 1
-			else:
-				degree_hist[G.degree(node)] += 1
-		#degrees = range(0, nx.number_of_nodes(G)+1,1)
-		degrees = range(1, nx.number_of_nodes(G)+1,1)		
-		keys = degree_hist.keys()	#keys of block
-		keys.sort
-		for item in degrees:
-			if item in keys:
-				P_k=float(degree_hist[item]) / float(nx.number_of_nodes(G))
-				check_sum +=P_k				
-				f.write('%d\t%f\t%d\t%f\n' % (item,R,degree_hist[item],P_k))
-				#1.node, 2.threshold, 3.degree hist, 4.degree distribution			
-			else:
-				f.write('%d\t%f\t0\t0.\n' % (item, R))
-		#f.write("\n")
-	f.close()
-
-def node_cc(input_mtx):   # cluster coefficient of each node
-	R = 0 
-	f = open(input_mtx[:-4]+'_node_cc.dat','w')			
-	#f.write('node\tr(thre.)\tnode_cc\n')
-	for i in range(0,101):
-		R = float(i)/100
-		G = get_threshold_matrix(input_mtx,R)
-		for node in G:
-			f.write("%d\t%f\t%f\n" % (node+1, R, nx.clustering(G,node)))
-			# node, threshold, clustering coefficient of node			
-		#f.write("\n")
-	f.close()
-
-def nodes_of_comp(input_mtx):		# connected components of nodes
-	R =0
-	f = open(input_mtx[:-4]+'_nodes_comp_.dat','w')
-	#f.write('node\tr(thre.)\tcount\n')
-	for i in range(0,101):
-		R = float(i)/100
-		G = get_threshold_matrix(input_mtx,R)
-		comps = nx.connected_component_subgraphs(G)
-		count = 0
-		for graph in comps:
-			count +=1
-			liste = graph.nodes()
-			for node in liste:
-				f.write("%d\t%f\t%d\n" % (node,R,count))
-				# 1.node, 2.threshold, 3. connected components		
-		#f.write("\n")
-	f.close
-
-def single_degrees(input_mtx): #degree (links) of each node
-	R = 0
-	f = open(input_mtx[:-4]+'_single_degrees.dat','w')	
-	for i in range(0,101):
-		#f.write('node\tr(thre.)\tdegree\n')
-		R = float(i)/100
-		G=get_threshold_matrix(input_mtx,R)
-		for node in G:
-			degree = G.degree(node)
-			f.write('%d\t%f\t%d\n' % ( (node+1), R, degree ) )
-			# 1.node, 2.threshold, 3.degree			
-		#f.write("\n")
-	f.close	
-
-
-
-def global_effic(input_mtx): 
-	R = 0
-	f = open(input_mtx[:-4]+'_global_efficiency.dat','w')
-	g = open(input_mtx[:-4]+'_node_global_efficiency.dat','w')
-	for i in range(0,101):
-		R = float(i)/100
-		G = get_threshold_matrix(input_mtx,R)
-		global_eff = 0.
-		for node_i in G:
-			sum_inverse_dist = 0.
-			for node_j in G:
-				if node_i != node_j:
-					if nx.has_path(G, node_i, node_j) == True:
-						sum_inverse_dist += 1. / nx.shortest_path_length(G, node_i, node_j)
-			A = sum_inverse_dist / nx.number_of_nodes(G)  # ?
-			g.write('%d\t%f\t%f\n' % ((node_i+1), R, A))
-			global_eff += sum_inverse_dist / (nx.number_of_nodes(G) - 1.) 
-		g.write("\n")
-		global_eff = global_eff / nx.number_of_nodes(G)
-		f.write("%f\t%f\n" % (R, global_eff))
-		#1.threshold, 2.global efficieny
-	f.close()  
-	g.close() 
-
-def local_effic(input_mtx):
-	R = 0
-	f = open(input_mtx[:-4]+'_local_efficency.dat','w')
-	g = open(input_mtx[:-4]+'_node_local_efficency.dat','w')
+	f = open(input_mtx[:-4]+'_local_efficency_ave.dat','w')
+	g = open(input_mtx[:-4]+'_local_efficency_node.dat','w')
 	#g.write('node\tr(thre.)\tlocal_eff')
 	for i in range(0,101):
 		R = float(i)/100
-		G = get_threshold_matrix(input_mtx,R)
+		G = get_graph(input_mtx,R)
 		local_effic = 0
 		for node_i in G:
 			hiwi = 0.	
@@ -225,14 +184,118 @@ def local_effic(input_mtx):
 	f.close()
 	g.close()
 
-def small_worldness(input_mtx):
+# get global efficiency for full network and single nodes separately
+def get_global_effic(input_mtx): 
+	R = 0
+	f = open(input_mtx[:-4]+'_global_efficiency_ave.dat','w')
+	g = open(input_mtx[:-4]+'_global_efficiency_node.dat','w')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		global_eff = 0.
+		for node_i in G:
+			sum_inverse_dist = 0.
+			for node_j in G:
+				if node_i != node_j:
+					if nx.has_path(G, node_i, node_j) == True:
+						sum_inverse_dist += 1. / nx.shortest_path_length(G, node_i, node_j)
+			A = sum_inverse_dist / nx.number_of_nodes(G)  # ?
+			g.write('%d\t%f\t%f\n' % ((node_i+1), R, A))
+			global_eff += sum_inverse_dist / (nx.number_of_nodes(G) - 1.) 
+		g.write("\n")
+		global_eff = global_eff / nx.number_of_nodes(G)
+		f.write("%f\t%f\n" % (R, global_eff))
+		#1.threshold, 2.global efficieny
+	f.close()  
+	g.close() 
+
+# get degree distribution P(k)
+def get_degree_distribution(input_mtx):			# degree distribution
+	R = 0
+	f = open(input_mtx[:-4]+'_degree_dist.dat', 'w')
+	#f.write('node\tr(thre.)\tdeg_hist\tdeg_dist\n')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		check_sum = 0.
+		degree_hist = {}
+		for node in G:
+			if G.degree(node) not in degree_hist:	
+				degree_hist[G.degree(node)] = 1
+			else:
+				degree_hist[G.degree(node)] += 1
+		#degrees = range(0, nx.number_of_nodes(G)+1,1)
+		degrees = range(1, nx.number_of_nodes(G)+1,1)		
+		keys = degree_hist.keys()	#keys of block
+		keys.sort
+		for item in degrees:
+			if item in keys:
+				P_k=float(degree_hist[item]) / float(nx.number_of_nodes(G))
+				check_sum +=P_k				
+				f.write('%d\t%f\t%d\t%f\n' % (item,R,degree_hist[item],P_k))
+				#1.node, 2.threshold, 3.degree hist, 4.degree distribution			
+			else:
+				f.write('%d\t%f\t0\t0.\n' % (item, R))
+		#f.write("\n")
+	f.close()
+
+# get clustering coefficient of each node
+def get_node_clustering_coefficient(input_mtx):   # cluster coefficient of each node
+	R = 0 
+	f = open(input_mtx[:-4]+'_cluster_coeffi_node.dat','w')			
+	#f.write('node\tr(thre.)\tnode_cc\n')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		for node in G:
+			f.write("%d\t%f\t%f\n" % (node+1, R, nx.clustering(G,node)))
+			# node, threshold, clustering coefficient of node			
+		#f.write("\n")
+	f.close()
+
+# get number of connected components of each node
+def get_connected_components_nodes(input_mtx):		# connected components of nodes
+	R =0
+	f = open(input_mtx[:-4]+'_connected_compo_node.dat','w')
+	#f.write('node\tr(thre.)\tcount\n')
+	for i in range(0,101):
+		R = float(i)/100
+		G = get_graph(input_mtx,R)
+		comps = nx.connected_component_subgraphs(G)
+		count = 0
+		for graph in comps:
+			count +=1
+			liste = graph.nodes()
+			for node in liste:
+				f.write("%d\t%f\t%d\n" % (node,R,count))
+				# 1.node, 2.threshold, 3. connected components		
+		#f.write("\n")
+	f.close
+
+# get degree of each node
+def get_degrees_node(input_mtx): #degree (links) of each node
+	R = 0
+	f = open(input_mtx[:-4]+'_degree_node.dat','w')	
+	for i in range(0,101):
+		#f.write('node\tr(thre.)\tdegree\n')
+		R = float(i)/100
+		G=get_graph(input_mtx,R)
+		for node in G:
+			degree = G.degree(node)
+			f.write('%d\t%f\t%d\n' % ( (node+1), R, degree ) )
+			# 1.node, 2.threshold, 3.degree			
+		#f.write("\n")
+	f.close	
+
+# get small worldness of network 
+def get_small_worldness(input_mtx):
 	R = 0
 	f = open(input_mtx[:-4]+'_small_worldness.dat','w')
 	g = open(input_mtx[:-4]+'_cc_trans_ER.dat','w')	
 	#g.write('r(thre.)\t\cc_A\tcc_ER\ttran_A\ttran_ER\n')	
 	for i in range(0,101):
 		R = float(i)/100
-		G = get_threshold_matrix(input_mtx, R)
+		G = get_graph(input_mtx, R)
 		ER_graph = nx.erdos_renyi_graph(nx.number_of_nodes(G), nx.density(G))
 		# erdos-renyi, binomial random graph generator ...(N,D:density)	
 		cluster = nx.average_clustering(G)   # clustering coef. of whole network
@@ -281,18 +344,18 @@ def small_worldness(input_mtx):
 
 	f.close() 
 	g.close()	 
-  #f.write = 1:threshold 2:cluster-coefficient 3:random-cluster-coefficient 4:shortest-pathlength 5:random-shortest-pathlength 6:transitivity 7:random-transitivity 8:S-Watts-Strogatz 9:S-transitivity" 
+  #1:threshold 2:cluster-coefficient 3:random-cluster-coefficient 4:shortest-pathlength 5:random-shortest-pathlength 6:transitivity 7:random-transitivity 8:S-Watts-Strogatz 9:S-transitivity" 
 
 
 def binomialCoefficient(n, k):
     return factorial(n) // (factorial(k) * factorial(n - k))
   
-def motifs(input_mtx):
+def get_motifs(input_mtx):
 	R = 0
 	f = open(input_mtx[:-4]+'_motifs.dat','w')
 	for i in range(0,101):
 		R = float(i)/100
-		G = get_threshold_matrix(input_mtx, R)
+		G = get_graph(input_mtx, R)
 		tri_dict = nx.triangles(G)   #number of triangles around nodes in G
 		summe = 0
 		for node in tri_dict:
@@ -317,22 +380,27 @@ def motifs(input_mtx):
 if __name__ == '__main__':
   usage = 'Usage: %s correlation_matrix threshold' % sys.argv[0]
   try:
-    test_network = sys.argv[1]
+    input_name = sys.argv[1]
     #value = sys.argv[2]
   except:
     print usage; sys.exit(1)
 
   ###manual choice of the threshold value
   #threshold = float(value)
-  #network = get_threshold_matrix(infilename_data, threshold)
-  measures_of_network(test_network)
-  shortest_path(test_network)
-  local_effic(test_network)
-  global_effic(test_network)
-  small_worldness(test_network)
-  degree_dist(test_network)	
-  node_cc(test_network)
-  node_cc(test_network)	
-  nodes_of_comp(test_network)
-  single_degrees(test_network)
-  motifs(test_network)	
+  #network = get_graph(input_name, threshold)
+  #get_print_adjacency_matrix(network)
+  #get_export_adjacency_matrix(network, input_name, threshold)
+  #get_characteristics(network, input_name, threshold)
+  #get_number_of_edges_and_density(input_name)
+  #get_average_cluster_coefficient(input_name)	
+  #get_degrees_ave(input_name)	
+  #get_connected_components(input_name)		
+  #get_local_efficiency(input_name)
+  #get_global_effic(input_name)
+  #get_degree_distribution(input_name)
+  #get_node_clustering_coefficient(input_name)
+  #get_connected_components_nodes(input_name)
+  #get_degrees_node(input_name)  
+  #get_shortest_pathway(input_name)
+  #get_small_worldness(input_name)	
+  #get_motifs(input_name)	
