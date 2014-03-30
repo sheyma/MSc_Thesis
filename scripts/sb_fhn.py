ccfbf1ea655f1f87cf23fd419d671b65b7209737
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2014-03-31 00:08:55 +0200

    FHN

--- scripts/sb_fhn.py
+++ scripts/sb_fhn.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pylab as pl
 import sys
@@ -9,10 +12,10 @@ eqns = {
 			}
 
 params = { 
-		 'gamma': 1.0, 
+		 'gamma': 0.9, 
 		 'alpha': 0.9,  
-         'b': 0.2,
-		 'TAU' : 1,	  
+         'b': 0.01,
+		 'TAU' : 1.25,	  
 		 'D' : 0} 								  # change noise strength
 
 noise = {'x': 'D * gwn()', 'y': 'D * gwn()'}
@@ -23,8 +26,8 @@ tfinal = 50
 
 dde.set_sim_params(tfinal)
 
-dde.hist_from_funcs({'x': lambda t: 1,
-					 'y': lambda t: 0})	  # initial conditions
+dde.hist_from_funcs({'x': lambda t: -1.2,
+					 'y': lambda t: -0.6})	  # initial conditions
 dde.run()								  # run the simulation
 
 sol_samp1 = dde.sample(0,50, 0.1)  		# sampled numerical solution	
@@ -35,6 +38,12 @@ t = sol_samp1['t']
 
 dde.hist
 
+
+x_limit = 2.5
+x_range = np.linspace(-2.5,2.5,500)
+# defining nullclines
+nullcl_01 = -( pow(x_range , 3) - params['gamma']*x_range)
+nullcl_02 = (params['alpha'] - x_range) / params['b']
 
 print "FitzHugh-Nagumo"
 print "variables : " , dde.vars
@@ -58,13 +67,17 @@ pl.plot(sol_samp1['t'],sol_samp1['x'],'r')
 pl.plot(sol_samp1['t'],sol_samp1['y'], 'b')
 pl.plot(sol_calc['t'],sol_calc['x'],'or')
 pl.plot(sol_calc['t'],sol_calc['y'],'ob')
+axis([-2 2 -1 1])
 pl.xlabel('$t$')
 pl.ylabel('$x_1,y_1$')
 
 pl.subplot(122)
 pl.plot(sol_samp1['x'],sol_samp1['y'], 'r')
 pl.plot(sol_samp1['x'][0],sol_samp1['y'][0], 'ok')
+pl.plot(x_range, nullcl_01, 'b')
+pl.plot(x_range, nullcl_02, 'k')
+pl.plot()
 pl.xlabel('$x$')
 pl.ylabel('$y$')
 
-#pl.show()
+pl.show()
--- scripts/sb_fhn_time_series.py
+++ scripts/sb_fhn_time_series.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pylab as pl
 import sys
