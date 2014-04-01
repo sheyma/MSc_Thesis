6bca896fc1b517791fbee669e0482df8ebee5dae
Şeyma Bayrak <seymaba@gmail.com>
2014-04-01 15:03:42 +0200

    fixed point by fsolve in python

--- scripts/sb_fhn.py
+++ scripts/sb_fhn.py
@@ -3,63 +3,63 @@
 
 import numpy as np
 import pylab as pl
+from pylab import *
 import sys
 from pydelay import dde23
 from scipy.optimize import fsolve
+from numpy import ma
 
+#FitzHugh-Naugmo 
 eqns = { 
-		 'x': '(y + gamma * x - pow(x,3.0)/3.0) * TAU',
+		 'x': '(y + gamma * x - pow(x,3.0)/3.0) * TAU',   
          'y': '- (x - alpha + b * y - I) / TAU'
 			}
 
 params = { 
 		 'gamma': 0.9, 
 		 'alpha': 1.3,  
-		 'b': -0.1,
+		 'b': -0.01,
 		 'TAU' : 10,	  
 		 'I' : -2.2,
 		 'D' : 0} 								  # change noise strength
 
 noise = {'x': 'D * gwn()', 'y': 'D * gwn()'}
 
-# defining nullclines
 x_limit = 2.5
 x_range = np.linspace(-2.5,2.5,500)
 
-nullcl_01 = (pow(x_range,3)/3 - params['gamma']*x_range)
-nullcl_02 = (params['alpha'] - x_range + params['I']) / params['b']
+# nullclines
+def nullcl_01(x):
+	return -(pow(x,3)/3 - params['gamma']*x)
+
+def nullcl_02(x):
+	return (params['alpha'] - x + params['I']) / params['b']
 
 # calculate the intersection of nullclines
 def intersection(x):
-  return ((pow(x,3)/3 - params['gamma']*x) - (params['alpha'] - x + params['I']) / params['b'] )
+  return nullcl_01(x) - nullcl_02(x)
 
 x0 = -1.5
 x_int = fsolve(intersection,x0)
-y_int = ( (pow(x_int ,3)/3) - params['gamma']*x_int)
-
+y_int = nullcl_01(x_int)
 
 dde = dde23(eqns=eqns, params=params, noise=noise) # initialize the solver
 
-tfinal = 50
+tfinal = 100
 
 dde.set_sim_params(tfinal)
 
-dde.hist_from_funcs({'x': lambda t: -0.99,
-		     'y': lambda t: -0.72})	  # initial conditions
+dde.hist_from_funcs({'x': lambda t: x_int,
+		             'y': lambda t: y_int})	  # initial conditions
 dde.run()								  # run the simulation
 
-sol_samp1 = dde.sample(0,50, 0.1)  		# sampled numerical solution	
+sol_samp1 = dde.sample(0, tfinal, 0.1)  		# sampled numerical solution	
 	
 sol_calc = dde.sol				   		# actual numerical solution
 
 t = sol_samp1['t']
 
 dde.hist
-
-
-
-
-
 
 print "FitzHugh-Nagumo"
 print "variables : " , dde.vars
@@ -68,6 +68,7 @@ print "equations : ", dde.eqns
 print "parameters : ", dde.params
 print "noise : ", dde.noise
 print "delays : ", dde.delays
+print "fixed point [x,y] : ", x_int, y_int
 
 f = open('deneme.dat','w')	
 
@@ -78,10 +79,7 @@ for i, t0 in enumerate(t):
 	f.write("\n")
 f.close()
 
-
-
-
-  
+print len(sol_samp1['t'][0:])
 pl.subplot(121)
 pl.plot(sol_samp1['t'],sol_samp1['x'],'r')
 pl.plot(sol_samp1['t'],sol_samp1['y'], 'b')
@@ -93,11 +91,30 @@ pl.ylabel('$x_1,y_1$')
 pl.subplot(122)
 pl.plot(sol_samp1['x'],sol_samp1['y'], 'r')
 pl.plot(sol_samp1['x'][0],sol_samp1['y'][0], 'ok')
-pl.plot(x_range, nullcl_01, 'b')
-pl.plot(x_range, nullcl_02, 'k')
-pl.plot(x_int,y_int,'or')
-pl.axis([-x_limit, x_limit, -1, 1])
+pl.plot(sol_calc['x'],sol_calc['y'],'or')
+pl.plot(x_range, nullcl_01(x_range), 'b')
+pl.plot(x_range, nullcl_02(x_range), 'k')
+pl.plot(x_int,y_int,'ok')
+
+X,Y = meshgrid( arange(-2,2,0.01),arange(-2,2,0.01) )
+U = nullcl_01(X)
+V = nullcl_02(Y)
+
+#Q = quiver( U, V)
+#qk = quiverkey(Q, 0.5, 0.92, 2, r'$2 \frac{m}{s}$', labelpos='W',
+          #     fontproperties={'weight': 'bold'})
+l,r,b,t = axis()
+dx, dy = r-l, t-b
+#axis([l-0.05*dx, r+0.05*dx, b-0.05*dy, t+0.05*dy])
+
+
+pl.axis([-2.3, 2.3, -1, 1])
 pl.xlabel('$x$')
 pl.ylabel('$y$')
 
+
 pl.show()
+
+
+
+
