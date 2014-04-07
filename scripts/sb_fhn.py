b9961bda1da529e33b2d57062ad7645ba0452413
Şeyma Bayrak <seymaba@gmail.com>
2014-04-07 16:50:13 +0200

    only local dynamics

--- scripts/sb_fhn.py
+++ scripts/sb_fhn.py
@@ -1,129 +1,97 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python2.7 
+
 # -*- coding: utf-8 -*-
 
+from __future__ import division
 import numpy as np
-import pylab as pl
 import sys
+import pylab as pl
 from pydelay import dde23
-from netpy import simnet
 from scipy.optimize import fsolve
 
-matrix_01 = sys.argv[1]		# test matrix
-matrix_02 = sys.argv[2]		# distances	
-
-G = np.loadtxt(matrix_01)
-d = np.loadtxt(matrix_02)
-
-#FitzHugh-Naugmo 
-eqns = { r'x{i}': '(y{i} + gamma * x{i} - pow(x{i},3.0)/3.0) * TAU  ',   
-         r'y{i}': '- (x{i} - alpha + b * y{i}) / TAU'
-			}
-
-params = { 
-		 'gamma': 0.9, 
-		 'alpha': 1.3,  
-		 'b': -0.01,
-		 'TAU' : 10,	  
-		 'D' : 0,
-		 'v' : 70.0,
-		 'sigma' : 0.5,    # C, coupling strength
-		 'K_constant' : 0.3}  # K, self coupling strength		  
-		 
-noise = {'x': 'D * gwn()', 'y': 'D * gwn()'}
-
-C = params['sigma']
-K = params['K_constant']
-
-H = [ [C,0] , [0,0]]
-
-T = d / params['v']
-
-
-
-coupling = '+{G:.12f}*{H}({self}(t-{tau})-{self})'
-
-neuronet = simnet(eqns, G, H, T, params, coupling, noise)
-
-#x_limit = 2.5
-#x_range = np.linspace(-2.5,2.5,500)
-
-## nullclines
-#def nullcl_01(x):
-	#return -(pow(x,3)/3 - params['gamma']*x)
-
-#def nullcl_02(x):
-	#return (params['alpha'] - x + params['I']) / params['b']
-
-## calculate the intersection of nullclines
-#def intersection(x):
-  #return nullcl_01(x) - nullcl_02(x)
-
-#x0 = -1.5
-#x_int = fsolve(intersection,x0)
-#y_int = nullcl_01(x_int)
-
-#dde = dde23(eqns=eqns, params=params, noise=noise) # initialize the solver
-
-#tfinal = 100
-
-#dde.set_sim_params(tfinal)
-
-#dde.hist_from_funcs({'x': lambda t: x_int,
-		             #'y': lambda t: y_int})	  # initial conditions
-#dde.run()						  # run the simulation
-
-#sol_samp1 = dde.sample(0, tfinal, 0.1)  		# sampled numerical solution	
+eqns = {r'x1' : '(y1 + gamma*x1 - pow(x1,3)/3.0) * TAU ',
+	r'y1' : '-(x1 - alpha + b*y1 ) / TAU',
+	r'x2' : '(y2 + gamma*x2 - pow(x2,3)/3.0) * TAU ',
+	r'y2' : '-(x2 - alpha + b*y2 ) / TAU'}
 	
-#sol_calc = dde.sol				   		# actual numerical solution
+params = {'gamma' : 0.85,
+	  'alpha' : 0.5,
+	  'TAU' : 10,
+	  'b' : -0.01,
+	  }
 
-#t = sol_samp1['t']
+dde = dde23(eqns=eqns,params=params)
 
-#dde.hist
+tfinal = 200
 
-print "FitzHugh-Nagumo"
-#print "variables : " , dde.vars
-#print len(t)
-#print "equations : ", dde.eqns
-#print "parameters : ", dde.params
-#print "noise : ", dde.noise
-#print "delays : ", dde.delays
-#print "fixed point [x,y] : ", x_int, y_int
+dde.set_sim_params(tfinal)
 
-#f = open('deneme.dat','w')	
+dde.hist_from_funcs( {'x1': lambda t : -1.2 , 'y1': lambda t: -0.5,
+		       'x2': lambda t : -1.8 , 'y2': lambda t: -0.1})
 
-#for i, t0 in enumerate(t):	
-	#f.write("%s\t" % (t0))
-	#f.write("%.5f\t" % (sol_samp1['x'][i]) )
-	#f.write("%.5f\t" % (sol_samp1['y'][i]))
-	#f.write("\n")
-#f.close()
+dde.run()
 
-#print len(sol_samp1['t'][0:])
-#pl.subplot(121)
-#pl.plot(sol_samp1['t'],sol_samp1['x'],'r')
-#pl.plot(sol_samp1['t'],sol_samp1['y'], 'b')
-#pl.plot(sol_calc['t'],sol_calc['x'],'or')
-#pl.plot(sol_calc['t'],sol_calc['y'],'ob')
-#pl.xlabel('$t$')
-#pl.ylabel('$x_1,y_1$')
+dt = float(sys.argv[1])
+sol_sample = dde.sample(0,tfinal,dt)
 
-#pl.subplot(122)
-#pl.plot(sol_samp1['x'],sol_samp1['y'], 'r')
-#pl.plot(sol_samp1['x'][0],sol_samp1['y'][0], 'ok')
-#pl.plot(sol_calc['x'],sol_calc['y'],'or')
-#pl.plot(x_range, nullcl_01(x_range), 'b')
-#pl.plot(x_range, nullcl_02(x_range), 'k')
-#pl.plot(x_int,y_int,'ok')
+t = sol_sample['t']
+x1 = sol_sample['x1']
+x2 = sol_sample['x2']
+y1 = sol_sample['y1']
+y2 = sol_sample['y2']
 
+print "FitzHugh Nagumo Local Dynamics"
+print "equations: ", dde.eqns
+print "parameters: ", dde.params
+print "simulation time : ", tfinal
+print "Sampling time interval : " , dt
 
+x_limit = 2.5
+x_range = np.linspace(-2.5,2.5,500)
 
-#pl.axis([-2.3, 2.3, -1, 1])
-#pl.xlabel('$x$')
-#pl.ylabel('$y$')
+# nullclines
+def nullcl_01(x):
+  return -(pow(x,3)/3 - params['gamma']*x)
 
+def nullcl_02(x):
+  return (params['alpha'] - x ) / params['b']
 
-#pl.show()
+# calculate the intersection of nullclines
+def intersection(x):
+  return nullcl_01(x) - nullcl_02(x)
+
+x0 = -2
+x_int = fsolve(intersection,x0)
+y_int = nullcl_01(x_int)
+
+print "intersection of nullclines (x_0, y_0): ", x_int, y_int
 
 
 
 
+pl.subplot(221)
+pl.plot(t, x1, 'r')
+pl.plot(t, y1, 'g')
+pl.xlabel('$t$')
+pl.ylabel('$x_1, y_1$')
+
+pl.subplot(222)
+pl.plot(x1, y1, 'r')
+pl.plot(x_range,nullcl_01(x_range),'b')
+pl.plot(x_range,nullcl_02(x_range),'k')
+pl.axis([-2.3, 2.3, -1, 1])
+pl.xlabel('$x_1$')
+pl.ylabel('$y_1$')
+
+pl.subplot(223)
+pl.plot(t, x2, 'r')
+pl.plot(t, y2, 'g')
+pl.xlabel('$t$')
+pl.ylabel('$x_2, y_2$')
+
+pl.subplot(224)
+pl.plot(x2, y2, 'g')
+pl.xlabel('$x_2$')
+pl.ylabel('$y_2$')
+
+pl.show()
