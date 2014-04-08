7714734a035de4e135fb271998878b6740c2b356
Şeyma Bayrak <s.bayrak.89@hotmail.com>
2014-04-08 21:39:16 +0200

    fhn model only local dynamics

--- scripts/sb_fhn.py
+++ scripts/sb_fhn.py
@@ -8,26 +8,44 @@ import sys
 import pylab as pl
 from pydelay import dde23
 from scipy.optimize import fsolve
+from pylab import *
 
-eqns = {r'x1' : '(y1 + gamma*x1 - pow(x1,3)/3.0) * TAU ',
-	r'y1' : '-(x1 - alpha + b*y1 ) / TAU',
-	r'x2' : '(y2 + gamma*x2 - pow(x2,3)/3.0) * TAU ',
-	r'y2' : '-(x2 - alpha + b*y2 ) / TAU'}
+eqns = {r'x' : '(y + gamma*x - pow(x,3)/3.0) * TAU ',
+	r'y' : '-(x - alpha + b*y +I ) / TAU' }
 	
-params = {'gamma' : 0.85,
-	  'alpha' : 0.5,
-	  'TAU' : 10,
+params = {'gamma' : 0.9,
+	  'alpha' : 1.6,
+	  'TAU' : 5,
 	  'b' : -0.01,
+	  'I' : 2.5
 	  }
+
+x_limit = 2.5
+x_range = np.linspace(-2.5,2.5,500)
+
+# nullclines
+def nullcl_01(X):
+  return -(pow(X,3)/3 - params['gamma']*X)
+  # make x minus
+  
+def nullcl_02(X):
+  return (params['alpha'] - X -params['I']) / params['b']
+
+# calculate the intersection of nullclines
+def intersection(X):
+  return nullcl_01(X) - nullcl_02(X)
+
+X0 = -2
+X_int = fsolve(intersection,X0)
+Y_int = nullcl_01(X_int)
 
 dde = dde23(eqns=eqns,params=params)
 
-tfinal = 200
+tfinal = 50
 
 dde.set_sim_params(tfinal)
 
-dde.hist_from_funcs( {'x1': lambda t : -1.2 , 'y1': lambda t: -0.5,
-		       'x2': lambda t : -1.8 , 'y2': lambda t: -0.1})
+dde.hist_from_funcs( {'x': lambda t : X_int , 'y': lambda t: Y_int })
 
 dde.run()
 
@@ -35,10 +53,9 @@ dt = float(sys.argv[1])
 sol_sample = dde.sample(0,tfinal,dt)
 
 t = sol_sample['t']
-x1 = sol_sample['x1']
-x2 = sol_sample['x2']
-y1 = sol_sample['y1']
-y2 = sol_sample['y2']
+x = sol_sample['x']
+y = sol_sample['y']
+
 
 print "FitzHugh Nagumo Local Dynamics"
 print "equations: ", dde.eqns
@@ -46,52 +63,25 @@ print "parameters: ", dde.params
 print "simulation time : ", tfinal
 print "Sampling time interval : " , dt
 
-x_limit = 2.5
-x_range = np.linspace(-2.5,2.5,500)
 
-# nullclines
-def nullcl_01(x):
-  return -(pow(x,3)/3 - params['gamma']*x)
-
-def nullcl_02(x):
-  return (params['alpha'] - x ) / params['b']
-
-# calculate the intersection of nullclines
-def intersection(x):
-  return nullcl_01(x) - nullcl_02(x)
-
-x0 = -2
-x_int = fsolve(intersection,x0)
-y_int = nullcl_01(x_int)
-
-print "intersection of nullclines (x_0, y_0): ", x_int, y_int
+print "intersection of nullclines (x_0, y_0): ", X_int, Y_int
 
 
+pl.subplot(121, xlabel='t', ylabel='x , y')
+pl.plot(t, x, 'r', label='$x(t)$')
+pl.plot(t, y, 'b', label='$y(t)$')
+#pl.xlabel('$t$')
+#pl.ylabel('$x, y$')
+lg = legend()
+lg.draw_frame(False)
 
-
-pl.subplot(221)
-pl.plot(t, x1, 'r')
-pl.plot(t, y1, 'g')
-pl.xlabel('$t$')
-pl.ylabel('$x_1, y_1$')
-
-pl.subplot(222)
-pl.plot(x1, y1, 'r')
-pl.plot(x_range,nullcl_01(x_range),'b')
-pl.plot(x_range,nullcl_02(x_range),'k')
+pl.subplot(122, xlabel='x', ylabel='y')
+pl.plot(x, y, 'r', label='$x,y$')
+pl.plot(x_range,nullcl_01(x_range),'b', label='$x_{nullcline}$')
+pl.plot(x_range,nullcl_02(x_range),'k',label='$y_{nullcline}$')
+pl.plot(X_int,Y_int, 'or', linewidth=3)
 pl.axis([-2.3, 2.3, -1, 1])
-pl.xlabel('$x_1$')
-pl.ylabel('$y_1$')
-
-pl.subplot(223)
-pl.plot(t, x2, 'r')
-pl.plot(t, y2, 'g')
-pl.xlabel('$t$')
-pl.ylabel('$x_2, y_2$')
-
-pl.subplot(224)
-pl.plot(x2, y2, 'g')
-pl.xlabel('$x_2$')
-pl.ylabel('$y_2$')
+lg = legend()
+lg.draw_frame(False)
 
 pl.show()
