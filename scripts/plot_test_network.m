c789b40c9b469eb29b3c1f088f1b8164872d3b7b
Şeyma Bayrak <seymaba@gmail.com>
2014-04-15 21:50:13 +0200

    minor corrections

--- scripts/plot_test_network.m
+++ scripts/plot_test_network.m
@@ -24,21 +24,21 @@ test_network = load('A_aal_single_network_measures.dat');
 input_name = 'A_aal_single_network_measures.dat';
 fig = figure(1);
 hold on
-set(gca,'FontSize',20)
-plot(test_network(:,1),test_network(:,3),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(test_network(:,1),test_network(:,3),'k','LineWidth',5)
 for i =1:length(random_G)
     a=strcat(input_name(1:6),'R',random_G(i),input_name(6:end));
     A=load(a);
     if color(i)=='g'
-        plot(A(:,1),A(:,3),strcat(color(i),'^'),'LineWidth',4) 
+        plot(A(:,1),A(:,3),strcat(color(i),'^'),'LineWidth',6) 
     else
-        plot(A(:,1),A(:,3),strcat(color(i),'o'),'LineWidth',3)
+        plot(A(:,1),A(:,3),strcat(color(i),'o'),'LineWidth',5)
     end
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Network Density')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Network Density','FontSize',35)
 hold off
 set(fig, 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -49,17 +49,17 @@ saveas(gcf,'Network_Density.eps','eps2c')
 % Network Clustering Coefficient
 figure(2);
 hold on
-set(gca,'FontSize',20)
-plot(test_network(:,1),test_network(:,4),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(test_network(:,1),test_network(:,4),'k','LineWidth',5)
 for i =1:length(random_G)
     a=strcat(input_name(1:6),'R',random_G(i),input_name(6:end));
     A=load(a);
-    plot(A(:,1),A(:,4),strcat(color(i),'o'),'LineWidth',3)    
+    plot(A(:,1),A(:,4),strcat(color(i),'o'),'LineWidth',5)    
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Clustering Coefficient')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Clustering Coefficient','FontSize',35)
 hold off
 set(figure(2), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -69,20 +69,20 @@ saveas(gcf,'Clustering_Coefficient.eps','eps2c')
 % Average degree of network
 figure(3);
 hold on
-set(gca,'FontSize',20)
-plot(test_network(:,1),test_network(:,5),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(test_network(:,1),test_network(:,5),'k','LineWidth',5)
 for i =1:length(random_G)
     A=load(strcat(input_name(1:6),'R',random_G(i),input_name(6:end)));
     if color(i) == 'g'
-        plot(A(:,1),A(:,5),strcat(color(i),'^'),'LineWidth',4)  
+        plot(A(:,1),A(:,5),strcat(color(i),'^'),'LineWidth',6)  
     else
-        plot(A(:,1),A(:,5),strcat(color(i),'o'),'LineWidth',3)
+        plot(A(:,1),A(:,5),strcat(color(i),'o'),'LineWidth',5)
     end        
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Average Degree')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Average Degree, <k>','FontSize',35)
 hold off
 set(figure(3), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -91,20 +91,20 @@ saveas(gcf,'Degree_Average.eps','eps2c')
 % Number of Connected Components
 figure(4);
 hold on
-set(gca,'FontSize',20)
-plot(test_network(:,1),test_network(:,6),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(test_network(:,1),test_network(:,6),'k','LineWidth',5)
 for i =1:length(random_G)
     A=load(strcat(input_name(1:6),'R',random_G(i),input_name(6:end)));
     if color(i) == 'g'
-        plot(A(:,1),A(:,6),strcat(color(i),'^'),'LineWidth',4)  
+        plot(A(:,1),A(:,6),strcat(color(i),'^'),'LineWidth',6)  
     else
-        plot(A(:,1),A(:,6),strcat(color(i),'o'),'LineWidth',3)
+        plot(A(:,1),A(:,6),strcat(color(i),'o'),'LineWidth',5)
     end    
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd','Location','NorthWest')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Average Connected Components')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Average Connected Components','FontSize',35)
 hold off
 set(figure(4), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -113,20 +113,20 @@ saveas(gcf,'Connected_Components_Average.eps','eps2c')
 % Shortest Pathway
 figure(5);
 hold on
-set(gca,'FontSize',20)
-plot(test_network(:,1),test_network(:,7),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(test_network(:,1),test_network(:,7),'k','LineWidth',5)
 for i =1:length(random_G)
     A=load(strcat(input_name(1:6),'R',random_G(i),input_name(6:end)));
     if color(i) == 'g'
-        plot(A(:,1),A(:,7),strcat(color(i),'^'),'LineWidth',4)  
+        plot(A(:,1),A(:,7),strcat(color(i),'^'),'LineWidth',6)  
     else
-        plot(A(:,1),A(:,7),strcat(color(i),'o'),'LineWidth',3)
+        plot(A(:,1),A(:,7),strcat(color(i),'o'),'LineWidth',5)
     end   
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Shortest Pathway')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Shortest Pathway','FontSize',35)
 hold off
 set(figure(5), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -136,21 +136,21 @@ saveas(gcf,'Shortest_Pathway.eps','eps2c')
 input_name_2 = 'A_aal_small_worldness.dat';
 SM = load('A_aal_small_worldness.dat');
 figure(6);
-set(gca,'FontSize',20)
-plot(SM(:,1),SM(:,8),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(SM(:,1),SM(:,8),'k','LineWidth',5)
 hold on
 for i = 1:length(random_G)
     A =load(strcat(input_name_2(1:6),'R',random_G(i),input_name_2(6:end)));
     if color(i) == 'g'
-        plot(A(:,1),A(:,8),strcat(color(i),'^'),'LineWidth',4)  
+        plot(A(:,1),A(:,8),strcat(color(i),'^'),'LineWidth',6)  
     else
-        plot(A(:,1),A(:,8),strcat(color(i),'o'),'LineWidth',3)
+        plot(A(:,1),A(:,8),strcat(color(i),'o'),'LineWidth',5)
     end 
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Small Worldness')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Small Worldness','FontSize',35)
 hold off
 set(figure(6), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -158,18 +158,18 @@ saveas(gcf,'Small_Worldness.eps','eps2c')
 
 % Transitivity
 figure(7);
-set(gca,'FontSize',20)
-plot(SM(:,1),SM(:,6),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(SM(:,1),SM(:,6),'k','LineWidth',5)
 hold on
 for i = 1:length(random_G)
     a = strcat(input_name_2(1:2),'R',random_G(i),input_name_2(2:end));
     A = load(strcat(input_name_2(1:6),'R',random_G(i),input_name_2(6:end)));
-    plot(A(:,1),A(:,6),strcat(color(i),'o'),'LineWidth',3)
+    plot(A(:,1),A(:,6),strcat(color(i),'o'),'LineWidth',5)
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Transitivity')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Transitivity','FontSize',35)
 hold off
 set(figure(7), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -179,22 +179,22 @@ saveas(gcf,'Transitivity.eps','eps2c')
 input_name_3 = 'A_aal_global_efficiency_ave.dat';
 GE = load('A_aal_global_efficiency_ave.dat');
 figure(8);
-set(gca,'FontSize',20)
-plot(GE(:,1),GE(:,2),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(GE(:,1),GE(:,2),'k','LineWidth',5)
 hold on
 for i = 1:length(random_G)
     a = strcat(input_name_3(1:6),'R',random_G(i),input_name_3(6:end));
     A = load(a);
     if color(i) == 'g'
-        plot(A(:,1),A(:,2),strcat(color(i),'^'),'LineWidth',4)  
+        plot(A(:,1),A(:,2),strcat(color(i),'^'),'LineWidth',6)  
     else
-        plot(A(:,1),A(:,2),strcat(color(i),'o'),'LineWidth',3)
+        plot(A(:,1),A(:,2),strcat(color(i),'o'),'LineWidth',5)
     end 
 end
 legend('Test Network', 'Ra','Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Average Global Efficiency')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Average Global Efficiency','FontSize',35)
 hold off
 set(figure(8), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -204,18 +204,18 @@ saveas(gcf,'Global_Efficiency_Average.eps','eps2c')
 input_name_4 = 'A_aal_local_efficiency_ave.dat';
 LE = load('A_aal_local_efficency_ave.dat');
 figure(9);
-set(gca,'FontSize',20)
-plot(LE(:,1),LE(:,2),'k','LineWidth',3)
+set(gca,'FontSize',30)
+plot(LE(:,1),LE(:,2),'k','LineWidth',5)
 hold on
 for i = 1:length(random_G)
    
     A =load(strcat(input_name_4(1:6),'R',random_G(i),'_local_efficency_ave.dat'));
-    plot(A(:,1),A(:,2),strcat(color(i),'o'),'LineWidth',3)
+    plot(A(:,1),A(:,2),strcat(color(i),'o'),'LineWidth',5)
 end
 legend('Test Network', 'Ra', 'Rb','Rc','Rd')
 legend('boxoff')
-xlabel('Threshold [r]')
-ylabel('Local Efficiency')
+xlabel('Threshold [r]','FontSize',35)
+ylabel('Local Efficiency','FontSize',35)
 hold off
 set(figure(9), 'units', 'inches','position',[10 10 30 20]) 
 set(gcf, 'PaperPositionMode','auto')
@@ -235,9 +235,9 @@ for i = 1:101
 end
 n=(1:90);
 m=(0:0.01:1);
-set(gca,'FontSize',15)
+set(gca,'FontSize',20)
 imagesc(n,m,z)
-colorbar
+h=colorbar; set(h,'fontsize',15);
 set(gca,'XTick',15:15:90);
 set(gca,'YTick',[0 0.25 0.50 0.75 1])
 set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
@@ -250,7 +250,7 @@ for i =1:4
     b_ = A(:,3);
 
     subplot(3,2,i+1)
-    set(gca,'FontSize',15)
+    set(gca,'FontSize',20)
     if random_G(i)~='d'
         b = zeros(101,90);
         for j = 1:101
@@ -262,12 +262,12 @@ for i =1:4
     else
         c = zeros(76,90);
         for j = 1:76
-            c(j,:) = c_( 90*(j-1)+1 : 90*j )';
+            c(j,:) = b_( 90*(j-1)+1 : 90*j )';
         end
         imagesc((1:1:90),(0.25:0.01:1),c)
         set(gca,'YTick',[0.25 0.45 0.65 0.85])
     end
-    colorbar
+    h=colorbar; set(h,'fontsize',15); set(h,'fontsize',15);
     set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
     set(gca,'XTick',15:15:90);
     xlabel('Nodes')
@@ -292,9 +292,9 @@ for i = 1:101
 end
 n=(1:90);
 m=(0:0.01:1);
-set(gca,'FontSize',15)
+set(gca,'FontSize',20)
 imagesc(n,m,z)
-colorbar
+h=colorbar; set(h,'fontsize',15);
 set(gca,'XTick',15:15:90);
 set(gca,'YTick',[0 0.25 0.50 0.75 1])
 set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
@@ -306,7 +306,7 @@ for i =1:4
     A = load(strcat(in_name_6(1:6),'R',random_G(i),in_name_6(6:end)));
     b_ = A(:,3);
     subplot(3,2,i+1)
-    set(gca,'FontSize',15)
+    set(gca,'FontSize',20)
     
     if random_G(i)~='d'
         b = zeros(101,90);
@@ -327,7 +327,7 @@ for i =1:4
     end    
     set(gca,'XTick',15:15:90);
     set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    colorbar
+    h=colorbar; set(h,'fontsize',15);
     xlabel('Nodes')
     ylabel('Threshold [r]')
     title(strcat('Local Efficiency of Nodes, R',random_G(i)),'FontSize',20)
@@ -352,9 +352,9 @@ for i = 1:101
 end
 n=(1:90);
 m=(0:0.01:1);
-set(gca,'FontSize',15)
+set(gca,'FontSize',20)
 imagesc(n,m,z)
-colorbar
+h=colorbar; set(h,'fontsize',15);
 set(gca,'XTick',15:15:90);
 set(gca,'YTick',[0 0.25 0.50 0.75 1])
 set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
@@ -366,7 +366,7 @@ for i =1:4
     A = load(strcat(in_name_7(1:6),'R',random_G(i),in_name_7(6:end)));
     b_ = A(:,3);
     subplot(3,2,i+1)
-    set(gca,'FontSize',15)
+    set(gca,'FontSize',20)
     
     if random_G(i)~='d'
         b = zeros(101,90);
@@ -387,7 +387,7 @@ for i =1:4
     end    
     set(gca,'XTick',15:15:90);
     set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    colorbar
+    h=colorbar; set(h,'fontsize',15);
     xlabel('Nodes')
     ylabel('Threshold [r]')
     title(strcat('Connected Component of Nodes, R',random_G(i)),'FontSize',20)
@@ -411,9 +411,9 @@ for i = 1:101
 end
 n=(1:90);
 m=(0:0.01:1);
-set(gca,'FontSize',15)
+set(gca,'FontSize',20)
 imagesc(n,m,z)
-colorbar
+h=colorbar; set(h,'fontsize',15);
 set(gca,'XTick',15:15:90);
 set(gca,'YTick',[0 0.25 0.50 0.75 1])
 set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
@@ -425,7 +425,7 @@ for i =1:4
     A = load(strcat(in_name_8(1:6),'R',random_G(i),in_name_8(6:end)));
     b_ = A(:,3);
     subplot(3,2,i+1)
-    set(gca,'FontSize',15)
+    set(gca,'FontSize',20)
     
     if random_G(i)~='d'
         b = zeros(101,90);
@@ -446,7 +446,7 @@ for i =1:4
     end    
     set(gca,'XTick',15:15:90);
     set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    colorbar
+    h=colorbar; set(h,'fontsize',15);
     xlabel('Nodes')
     ylabel('Threshold [r]')
     title(strcat('Degree Distribution, R',random_G(i)),'FontSize',20)
@@ -469,9 +469,9 @@ for i = 1:101
 end
 n=(1:90);
 m=(0:0.01:1);
-set(gca,'FontSize',15)
+set(gca,'FontSize',20)
 imagesc(n,m,z)
-colorbar
+h=colorbar; set(h,'fontsize',15);
 set(gca,'XTick',15:15:90);
 set(gca,'YTick',[0 0.25 0.50 0.75 1])
 set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
@@ -483,7 +483,7 @@ for i =1:4
     A = load(strcat(in_name_9(1:6),'R',random_G(i),in_name_9(6:end)));
     b_ = A(:,3);
     subplot(3,2,i+1)
-    set(gca,'FontSize',15)
+    set(gca,'FontSize',20)
     
     if random_G(i)~='d'
         b = zeros(101,90);
@@ -492,7 +492,8 @@ for i =1:4
             b(j,:) = b_( ( 90*a+1 :(90*a+90) ),:);
         end
         imagesc(n,m,b)
-        set(gca,'YTick',[0 0.25 0.50 0.75 1])        
+        set(gca,'YTick',[0 0.25 0.50 0.75 1]); set(h,'YTick',0:0.2:1);
+        h=colorbar; set(h,'fontsize',15); 
     else
         c = zeros(76,90);
         for j = 1:76
@@ -500,11 +501,10 @@ for i =1:4
         end
             imagesc((1:1:90),(0.25:0.01:1),c)
             set(gca,'YTick',[0.25 0.45 0.65 0.85])
-
+            h=colorbar; set(h,'fontsize',15); set(h,'YTick',0:0.2:1);
     end    
     set(gca,'XTick',15:15:90);
     set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    colorbar
     xlabel('Nodes')
     ylabel('Threshold [r]')
     title(strcat('Clustering Coefficient of Node, R',random_G(i)),'FontSize',20)
