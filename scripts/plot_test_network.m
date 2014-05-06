c347b24904d6178c01c5863e15ac8274bfb84ed1
Şeyma Bayrak <seymaba@gmail.com>
2014-05-06 15:41:01 +0200

    logarithmic degree distribution of networks

--- scripts/plot_test_network.m
+++ scripts/plot_test_network.m
@@ -10,7 +10,9 @@
               %...7:random-transitivity 8:S-Watts-Strogatz 9:S-transitivity
 % A_*_global_efficiency_ave.dat : #1.threshold, 2.global efficieny
 % A_*_local_efficency_ave.dat : # 1.threshold, 2.local efficiency
-% A_*_global_efficiency_node.dat : #1.node, 2,threshold, 3.glo. effi.of node              
+% A_*_assortativity.dat : # 1. threshold, 2.assortativity coefficient
+% A_*_global_efficiency_node.dat : #1.node, 2,threshold, 3.glo. effi.of node  
+
 
 % A_*_degree_dist.dat : #1.node, 2.threshold, 3.degree hist, 4.degree distr.
 % A_*_connected_compo_node.dat : # 1.node, 2.threshold, 3. connected compon.
@@ -18,6 +20,7 @@
 %                                of each node, 4. degree of node
 
 % Network Density
+%random_G = ('0abfdc');
 random_G = ('0abfdc');
 color='kmbcyr'; type = '-*o+*o';
 input_name = 'A_aal_single_network_measures.dat';
@@ -105,7 +108,7 @@ for i =1:length(random_G)
     A=load(strcat(input_name(1:6),'R',random_G(i),input_name(6:end)));
     plot(A(:,1),A(:,7),strcat(color(i),type(i)),'LineWidth',3) 
 end
-legend('Test Network', 'Ra','Rb','Rf','Rc','Location','NorthWest')
+legend('Test Network', 'Ra','Rb','Rf','Rd','Rc','Location','NorthWest')
 set(legend,'FontSize',14)
 legend('boxoff')
 xlabel('Threshold [r]')
@@ -124,7 +127,7 @@ for i = 1:length(random_G)
     A =load(strcat(input_name_2(1:6),'R',random_G(i),input_name_2(6:end)));
     plot(A(:,1),A(:,8),strcat(color(i),type(i)),'LineWidth',3)  
 end
-legend('Test Network', 'Ra','Rb','Rf','Rc','Location','NorthWest')
+legend('Test Network', 'Ra','Rb','Rf','Rd','Rc','Location','NorthWest')
 set(legend,'FontSize',14)
 legend('boxoff')
 xlabel('Threshold [r]')
@@ -143,7 +146,7 @@ for i = 1:length(random_G)
     A = load(strcat(input_name_2(1:6),'R',random_G(i),input_name_2(6:end)));
     plot(A(:,1),A(:,6),strcat(color(i),type(i)),'LineWidth',3)
 end
-legend('Test Network', 'Ra','Rb','Rf','Rc','Location','SouthWest')
+legend('Test Network', 'Ra','Rb','Rf','Rd','Rc','Location','SouthWest')
 set(legend,'FontSize',14)
 legend('boxoff')
 xlabel('Threshold [r]')
@@ -163,7 +166,7 @@ for i = 1:length(random_G)
     A = load(a);
     plot(A(:,1),A(:,2),strcat(color(i),type(i)),'LineWidth',3)  
 end
-legend('Test Network', 'Ra','Rb','Rf','Rc','Location','SouthWest')
+legend('Test Network', 'Ra','Rb','Rf','Rd','Rc','Location','SouthWest')
 set(legend,'FontSize',14)
 legend('boxoff')
 xlabel('Threshold [r]')
@@ -182,7 +185,7 @@ for i = 1:length(random_G)
     A =load(strcat(input_name_4(1:6),'R',random_G(i),'_local_efficency_ave.dat'));
     plot(A(:,1),A(:,2),strcat(color(i),type(i)),'LineWidth',3)
 end
-legend('Test Network', 'Ra','Rb','Rf','Rc','Location','SouthWest')
+legend('Test Network', 'Ra','Rb','Rf','Rd','Rc','Location','SouthWest')
 set(legend,'FontSize',14)
 legend('boxoff')
 xlabel('Threshold [r]')
@@ -192,295 +195,288 @@ set(figure(9), 'units', 'inches','position',[5 4 10 7])
 set(gcf, 'PaperPositionMode','auto')
 %saveas(gcf,'Local_Efficiency_Average.eps','eps2c')
 
+% Assortativity coefficient of network
+in_name_5 = 'A_aal_assortativity.dat';
+figure(10);
+set(gca,'FontSize',25)
+hold on
+for i = 1:length(random_G)
+    A = load(strcat(in_name_5(1:6),'R',random_G(i),in_name_5(6:end)));
+    plot(A(:,1),A(:,2),strcat(color(i),type(i)), 'Linewidth', 3)
+end
+legend('Test Network', 'Ra','Rb','Rf','Rd','Rc','Location','NorthWest')
+set(legend,'FontSize',14)
+legend('boxoff')
+xlabel('Threshold [r]')
+ylabel('Assortativity')
+hold off
+set(figure(10), 'units', 'inches','position',[5 4 10 7]) 
+set(gcf, 'PaperPositionMode','auto')
+%saveas(gcf,'Assortativity.eps','eps2c')
+
 % DISTRIBUTIONS
 % Global efficiency of nodes
-GEF = load('A_aal_global_efficiency_node.dat');
 in_name_5 = 'A_aal_global_efficiency_node.dat';
-z_ = GEF(:,3);
-z = zeros(101,90);
-figure(10);
-subplot(3,2,1)
-for i = 1:101
-    a = i-1;
-   z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
-end
-n=(1:90);
-m=(0:0.01:1);
-set(gca,'FontSize',20)
-imagesc(n,m,z)
-h=colorbar; set(h,'fontsize',15);
-set(gca,'XTick',15:15:90);
-set(gca,'YTick',[0 0.25 0.50 0.75 1])
-set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-xlabel('Nodes')
-ylabel('Threshold [r]')
-title('Global Efficiency of Nodes (Test Network)','FontSize',20)
+figure(11);
 
-for i =1:4
-    A = load(strcat(in_name_5(1:6),'R',random_G(i),in_name_5(6:end)));
-    b_ = A(:,3);
-
-    subplot(3,2,i+1)
+for j = 1:length(random_G)
+   
+    a =strcat(in_name_5(1:6),'R',random_G(j),in_name_5(6:end));
+    GEF = load(a);
+    z_ = GEF(:,3);
+    z = zeros(101,90);
+    
+    subplot(3,2,j)
     set(gca,'FontSize',20)
-    if random_G(i)~='d'
-        b = zeros(101,90);
-        for j = 1:101
-            a = j-1;
-            b(j,:) = b_( ( 90*a+1 :(90*a+90) ),:);
+    if random_G(j) ~= 'd' && random_G(j) ~='c' 
+        for i = 1:101
+            a = i-1;
+           z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
         end
-        imagesc(n,m,b)
+        n=(1:90);
+        m=(0:0.01:1);
+        imagesc(n,m,z)
+        h=colorbar; set(h,'fontsize',15);
         set(gca,'YTick',[0 0.25 0.50 0.75 1])
-    else
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    elseif random_G(j)=='d'
         c = zeros(76,90);
-        for j = 1:76
-            c(j,:) = b_( 90*(j-1)+1 : 90*j )';
+        for k = 1:76
+            c(k,:) = z_( 90*(k-1)+1 : 90*k )';
         end
         imagesc((1:1:90),(0.25:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
         set(gca,'YTick',[0.25 0.45 0.65 0.85])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))    
+    elseif random_G(j) == 'c'
+        c = zeros(42,90);
+        for k = 1:42
+            c(k,:) = z_( 90*(k-1+5)+1 : 90*(k+5) )';
+        end
+        imagesc((1:1:90),(0.59:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.6 0.68 0.80 0.92 1.00])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
     end
-    h=colorbar; set(h,'fontsize',15); set(h,'fontsize',15);
-    set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
     set(gca,'XTick',15:15:90);
     xlabel('Nodes')
     ylabel('Threshold [r]')
-    title(strcat('Global Efficiency of Nodes, R',random_G(i)),'FontSize',20)
-end
-        
-set(figure(10), 'units', 'inches','position',[10 10 15 20]) 
-set(gcf, 'PaperPositionMode','auto')
-saveas(gcf,'Global_Efficiency_Nodes.eps','eps2c')
-
-% Local efficiency of nodes
-LEF = load('A_aal_local_efficency_node.dat');
-in_name_6 = 'A_aal_local_efficency_node.dat';
-z_ = LEF(:,3);
-z = zeros(101,90);
-figure(11);
-subplot(3,2,1)
-for i = 1:101
-    a = i-1;
-   z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
-end
-n=(1:90);
-m=(0:0.01:1);
-set(gca,'FontSize',20)
-imagesc(n,m,z)
-h=colorbar; set(h,'fontsize',15);
-set(gca,'XTick',15:15:90);
-set(gca,'YTick',[0 0.25 0.50 0.75 1])
-set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-xlabel('Nodes')
-ylabel('Threshold [r]')
-title('Local Efficiency of Nodes (Test Network)','FontSize',20)
-
-for i =1:4
-    A = load(strcat(in_name_6(1:6),'R',random_G(i),in_name_6(6:end)));
-    b_ = A(:,3);
-    subplot(3,2,i+1)
-    set(gca,'FontSize',20)
+    title(strcat('Global Efficiency of Nodes, R', random_G(j)))
     
-    if random_G(i)~='d'
-        b = zeros(101,90);
-        for j = 1:101
-            a = j-1;
-            b(j,:) = b_( ( 90*a+1 :(90*a+90) ),:);
-        end
-        imagesc(n,m,b)
-        set(gca,'YTick',[0 0.25 0.50 0.75 1])        
-    else
-        c = zeros(76,90);
-        for j = 1:76
-            c(j,:) = b_( 90*(j-1)+1 : 90*j )';
-        end
-            imagesc((1:1:90),(0.25:0.01:1),c)
-            set(gca,'YTick',[0.25 0.45 0.65 0.85])
+end
+set(figure(11), 'units', 'inches','position',[10 10 13 20]) 
+set(gcf, 'PaperPositionMode','auto')
+%saveas(gcf,'Global_Efficiency_Nodes.eps','eps2c')
 
-    end    
+%Local efficiency of nodes
+in_name_6 = 'A_aal_local_efficency_node.dat';
+figure(12);
+
+for j = 1:length(random_G)
+   
+    a =strcat(in_name_6(1:6),'R',random_G(j),in_name_6(6:end));
+    LEF = load(a);
+    z_ = LEF(:,3);
+    z = zeros(101,90);
+    
+    subplot(3,2,j)
+    set(gca,'FontSize',20)
+    if random_G(j) ~= 'd' && random_G(j) ~='c' 
+        for i = 1:101
+            a = i-1;
+           z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
+        end
+        n=(1:90);
+        m=(0:0.01:1);
+        imagesc(n,m,z)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0 0.25 0.50 0.75 1])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    elseif random_G(j)=='d'
+        c = zeros(76,90);
+        for k = 1:76
+            c(k,:) = z_( 90*(k-1)+1 : 90*k )';
+        end
+        imagesc((1:1:90),(0.25:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.25 0.45 0.65 0.85])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))    
+    elseif random_G(j) == 'c'
+        c = zeros(42,90);
+        for k = 1:42
+            c(k,:) = z_( 90*(k-1+5)+1 : 90*(k+5) )';
+        end
+        imagesc((1:1:90),(0.59:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.6 0.68 0.80 0.92 1.00])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    end
     set(gca,'XTick',15:15:90);
-    set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    h=colorbar; set(h,'fontsize',15);
     xlabel('Nodes')
     ylabel('Threshold [r]')
-    title(strcat('Local Efficiency of Nodes, R',random_G(i)),'FontSize',20)
+    title(strcat('Local Efficiency of Nodes, R', random_G(j)))
+    
 end
-
-set(figure(11), 'units', 'inches','position',[10 10 15 20]) 
+set(figure(12), 'units', 'inches','position',[10 10 13 20]) 
 set(gcf, 'PaperPositionMode','auto')
-saveas(gcf,'Local_Efficiency_Nodes.eps','eps2c')
+%saveas(gcf,'Local_Efficiency_Nodes.eps','eps2c')
+
 
 % Connected Components of Nodes
-
-% Local efficiency of nodes
-COM = load('A_aal_connected_compo_node.dat');
 in_name_7 = 'A_aal_connected_compo_node.dat';
-z_ = COM(:,3);
-z = zeros(101,90);
-figure(12);
-subplot(3,2,1)
-for i = 1:101
-    a = i-1;
-   z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
-end
-n=(1:90);
-m=(0:0.01:1);
-set(gca,'FontSize',20)
-imagesc(n,m,z)
-h=colorbar; set(h,'fontsize',15);
-set(gca,'XTick',15:15:90);
-set(gca,'YTick',[0 0.25 0.50 0.75 1])
-set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-xlabel('Nodes')
-ylabel('Threshold [r]')
-title('Connected Component of Nodes (Test Network)','FontSize',20)
-
-for i =1:4
-    A = load(strcat(in_name_7(1:6),'R',random_G(i),in_name_7(6:end)));
-    b_ = A(:,3);
-    subplot(3,2,i+1)
-    set(gca,'FontSize',20)
+figure(13);
+for j = 1:length(random_G)
+   
+    a =strcat(in_name_7(1:6),'R',random_G(j),in_name_7(6:end));
+    COM = load(a);
+    z_ = COM(:,3);
+    z = zeros(101,90);
     
-    if random_G(i)~='d'
-        b = zeros(101,90);
-        for j = 1:101
-            a = j-1;
-            b(j,:) = b_( ( 90*a+1 :(90*a+90) ),:);
+    subplot(3,2,j)
+    set(gca,'FontSize',15)
+    if random_G(j) ~= 'd' && random_G(j) ~='c' 
+        for i = 1:101
+            a = i-1;
+           z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
         end
-        imagesc(n,m,b)
-        set(gca,'YTick',[0 0.25 0.50 0.75 1])        
-    else
+        n=(1:90);
+        m=(0:0.01:1);
+        imagesc(n,m,z)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0 0.25 0.50 0.75 1])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    elseif random_G(j)=='d'
         c = zeros(76,90);
-        for j = 1:76
-            c(j,:) = b_( 90*(j-1)+1 : 90*j )';
+        for k = 1:76
+            c(k,:) = z_( 90*(k-1)+1 : 90*k )';
         end
-            imagesc((1:1:90),(0.25:0.01:1),c)
-            set(gca,'YTick',[0.25 0.45 0.65 0.85])
-
-    end    
+        imagesc((1:1:90),(0.25:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.25 0.45 0.65 0.85])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))    
+    elseif random_G(j) == 'c'
+        c = zeros(42,90);
+        for k = 1:42
+            c(k,:) = z_( 90*(k-1+5)+1 : 90*(k+5) )';
+        end
+        imagesc((1:1:90),(0.59:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.6 0.68 0.80 0.92 1.00])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    end
     set(gca,'XTick',15:15:90);
-    set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    h=colorbar; set(h,'fontsize',15);
     xlabel('Nodes')
     ylabel('Threshold [r]')
-    title(strcat('Connected Component of Nodes, R',random_G(i)),'FontSize',20)
+    title(strcat('Connected Components of Nodes, R', random_G(j)), 'FontSize',20)
+    
 end
-
-set(figure(12), 'units', 'inches','position',[10 10 15 20]) 
+set(figure(13), 'units', 'inches','position',[10 10 13 20]) 
 set(gcf, 'PaperPositionMode','auto')
-saveas(gcf,'Connected_Components_Nodes.eps','eps2c')
-
+%saveas(gcf,'Connected_Components_Nodes.eps','eps2c')
 
 % Degree Distribution
-DD = load('A_aal_degree_dist.dat');
 in_name_8 = 'A_aal_degree_dist.dat';
-z_ = DD(:,3);
-z = zeros(101,90);
-figure(13);
-subplot(3,2,1)
-for i = 1:101
-    a = i-1;
-   z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
-end
-n=(1:90);
-m=(0:0.01:1);
-set(gca,'FontSize',20)
-imagesc(n,m,z)
-h=colorbar; set(h,'fontsize',15);
-set(gca,'XTick',15:15:90);
-set(gca,'YTick',[0 0.25 0.50 0.75 1])
-set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-xlabel('Nodes')
-ylabel('Threshold [r]')
-title('Degree Distribution (Test Network)','FontSize',20)
-
-for i =1:4
-    A = load(strcat(in_name_8(1:6),'R',random_G(i),in_name_8(6:end)));
-    b_ = A(:,3);
-    subplot(3,2,i+1)
-    set(gca,'FontSize',20)
+figure(14);
+for j = 1:length(random_G)
+   
+    a =strcat(in_name_8(1:6),'R',random_G(j),in_name_8(6:end));
+    DD = load(a);
+    z_ = DD(:,3);
+    z = zeros(101,90);
     
-    if random_G(i)~='d'
-        b = zeros(101,90);
-        for j = 1:101
-            a = j-1;
-            b(j,:) = b_( ( 90*a+1 :(90*a+90) ),:);
+    subplot(3,2,j)
+    set(gca,'FontSize',15)
+    if random_G(j) ~= 'd' && random_G(j) ~='c' 
+        for i = 1:101
+            a = i-1;
+           z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
         end
-        imagesc(n,m,b)
-        set(gca,'YTick',[0 0.25 0.50 0.75 1])        
-    else
+        n=(1:90);
+        m=(0:0.01:1);
+        imagesc(log10(n),log10(m),log10(z))
+        h=colorbar; set(h,'fontsize',15);
+        %set(gca,'YTick',[0 0.25 0.50 0.75 1])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    elseif random_G(j)=='d'
         c = zeros(76,90);
-        for j = 1:76
-            c(j,:) = b_( 90*(j-1)+1 : 90*j )';
+        for k = 1:76
+            c(k,:) = z_( 90*(k-1)+1 : 90*k )';
         end
-            imagesc((1:1:90),(0.25:0.01:1),c)
-            set(gca,'YTick',[0.25 0.45 0.65 0.85])
-
-    end    
+        imagesc((1:1:90),(0.25:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.25 0.45 0.65 0.85])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))    
+    elseif random_G(j) == 'c'
+        c = zeros(42,90);
+        for k = 1:42
+            c(k,:) = z_( 90*(k-1+5)+1 : 90*(k+5) )';
+        end
+        imagesc((1:1:90),(0.59:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.6 0.68 0.80 0.92 1.00])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    end
     set(gca,'XTick',15:15:90);
-    set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-    h=colorbar; set(h,'fontsize',15);
     xlabel('Nodes')
     ylabel('Threshold [r]')
-    title(strcat('Degree Distribution, R',random_G(i)),'FontSize',20)
+    title(strcat('Degree Distribution, R', random_G(j)), 'FontSize',20)
+    
 end
-
-set(figure(13), 'units', 'inches','position',[10 10 15 20]) 
+set(figure(14), 'units', 'inches','position',[10 10 13 20]) 
 set(gcf, 'PaperPositionMode','auto')
-saveas(gcf,'Degree_Distribution.eps','eps2c')
+%saveas(gcf,'Degree_Distribution.eps','eps2c')
+
+
 
 % Clustering Coeficient of Nodes
-cc_node = load('A_aal_cc_and_degree_node.dat');
-in_name_9 = 'A_aal_cc_and_degree_node.dat';
-z_ = cc_node(:,3);
-z = zeros(101,90);
-figure(14);
-subplot(3,2,1)
-for i = 1:101
-    a = i-1;
-   z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
-end
-n=(1:90);
-m=(0:0.01:1);
-set(gca,'FontSize',20)
-imagesc(n,m,z)
-h=colorbar; set(h,'fontsize',15);
-set(gca,'XTick',15:15:90);
-set(gca,'YTick',[0 0.25 0.50 0.75 1])
-set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
-xlabel('Nodes')
-ylabel('Threshold [r]')
-title('Clustering Coefficient of Node (TN)','FontSize',20)
-
-for i =1:4
-    A = load(strcat(in_name_9(1:6),'R',random_G(i),in_name_9(6:end)));
-    b_ = A(:,3);
-    subplot(3,2,i+1)
-    set(gca,'FontSize',20)
+in_name_8 = 'A_aal_cc_and_degree_node.dat';
+figure(15);
+for j = 1:length(random_G)
+   
+    a =strcat(in_name_8(1:6),'R',random_G(j),in_name_8(6:end));
+    DD = load(a);
+    z_ = DD(:,3);
+    z = zeros(101,90);
     
-    if random_G(i)~='d'
-        b = zeros(101,90);
-        for j = 1:101
-            a = j-1;
-            b(j,:) = b_( ( 90*a+1 :(90*a+90) ),:);
+    subplot(3,2,j)
+    set(gca,'FontSize',15)
+    if random_G(j) ~= 'd' && random_G(j) ~='c' 
+        for i = 1:101
+            a = i-1;
+           z(i,:) = z_( ( 90*a+1 :(90*a+90) ),:);
         end
-        imagesc(n,m,b)
-        set(gca,'YTick',[0 0.25 0.50 0.75 1]); set(h,'YTick',0:0.2:1);
-        h=colorbar; set(h,'fontsize',15); 
-    else
+        n=(1:90);
+        m=(0:0.01:1);
+        imagesc(n,m,z)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0 0.25 0.50 0.75 1])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    elseif random_G(j)=='d'
         c = zeros(76,90);
-        for j = 1:76
-            c(j,:) = b_( 90*(j-1)+1 : 90*j )';
+        for k = 1:76
+            c(k,:) = z_( 90*(k-1)+1 : 90*k )';
         end
-            imagesc((1:1:90),(0.25:0.01:1),c)
-            set(gca,'YTick',[0.25 0.45 0.65 0.85])
-            h=colorbar; set(h,'fontsize',15); set(h,'YTick',0:0.2:1);
-    end    
+        imagesc((1:1:90),(0.25:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.25 0.45 0.65 0.85])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))    
+    elseif random_G(j) == 'c'
+        c = zeros(42,90);
+        for k = 1:42
+            c(k,:) = z_( 90*(k-1+5)+1 : 90*(k+5) )';
+        end
+        imagesc((1:1:90),(0.59:0.01:1),c)
+        h=colorbar; set(h,'fontsize',15);
+        set(gca,'YTick',[0.6 0.68 0.80 0.92 1.00])
+        set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
+    end
     set(gca,'XTick',15:15:90);
-    set(gca, 'YTickLabel', num2str(get(gca,'YTick')','%.2f'))
     xlabel('Nodes')
     ylabel('Threshold [r]')
-    title(strcat('Clustering Coefficient of Node, R',random_G(i)),'FontSize',20)
+    title(strcat('Clustering Coedfficient of Nodes, R', random_G(j)), 'FontSize',20)
+    
 end
-
-set(figure(14), 'units', 'inches','position',[10 10 15 20]) 
+set(figure(15), 'units', 'inches','position',[10 10 13 20]) 
 set(gcf, 'PaperPositionMode','auto')
-saveas(gcf,'Clustering_Coefficient_Node.eps','eps2c')
+%saveas(gcf,'Clustering_Coefficient_Node.eps','eps2c')
+
