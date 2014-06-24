ccbcf000c4e537a1d6be69d60063b058272f14ee
Şeyma Bayrak <seymaba@gmail.com>
2014-06-24 16:43:07 +0200

    partial randomization of input matrix in fraction [0,1]

--- scripts/export_randomized.m
+++ scripts/export_randomized.m
@@ -14,13 +14,16 @@ for i = 48:66  % has to be chosen manually
     %filename_ir =strcat('A_aal_ir_ADJ_thr_0.', num2str(i), '.dat');
     %dlmwrite(filename_ir, ir_adj_mtx, 'delimiter','\t', 'precision', 1);
     
-    jr_adj_mtx = randmio_und_connected(adj_mtx , round(iter/10));
-    filename_jr =strcat('A_aal_jr_ADJ_thr_0.', num2str(i), '.dat');
-    dlmwrite(filename_jr, jr_adj_mtx, 'delimiter','\t', 'precision', 1);
+%     jr_adj_mtx = randmio_und_connected(adj_mtx , round(iter/10));
+%     filename_jr =strcat('A_aal_jr_ADJ_thr_0.', num2str(i), '.dat');
+%     dlmwrite(filename_jr, jr_adj_mtx, 'delimiter','\t', 'precision', 1);
 
-    
-    %kr_adj_mtx = randomize_graph_partial(----------adj_mtx , )
-    
+    for alpha = 0.1: 0.1 : 1;
+        a = num2str(alpha);
+        kr_adj_mtx = randomizer_bin_und(adj_mtx, alpha );
+        filename_kr = strcat('A_aal_kr_ADJ_thr_0.', num2str(i), '_alpha_', a, '.dat');
+        dlmwrite(filename_kr, kr_adj_mtx, 'delimiter','\t', 'precision', 1);
+    end    
     %lr_adj_mtx = randomizer_bin_und(......)
     
         
