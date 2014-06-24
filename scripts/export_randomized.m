518cfdd78589dcfe407e080260c703bbd84cd99d
Şeyma Bayrak <seymaba@gmail.com>
2014-06-24 15:47:24 +0200

    run randmio_connected_und.m in script for different thr.

--- scripts/export_randomized.m
+++ scripts/export_randomized.m
@@ -1,6 +1,6 @@
 
 % iteration required for randmio_und.m
-iter = 100;
+iter = 1000;
 % partly define the name of adjacency matrices
 name = 'A_aal_0_ADJ_thr_0.';
 for i = 48:66  % has to be chosen manually
@@ -8,9 +8,20 @@ for i = 48:66  % has to be chosen manually
     input = strcat(name, num2str(i), '.dat' );
     % load the adjacency matrix
     adj_mtx = load(input);
+    
     % randomize the adjacency matrix
-    new_adj_mtx = randmio_und(adj_mtx , iter);    
-    filename =strcat('A_aal_ir_ADJ_thr_0.', num2str(i), '.dat');
-    % save randomized adjacency matrix as -ascii
-    dlmwrite(filename, new_adj_mtx, 'delimiter','\t', 'precision', 1);
+    %ir_adj_mtx = randmio_und(adj_mtx , iter);    
+    %filename_ir =strcat('A_aal_ir_ADJ_thr_0.', num2str(i), '.dat');
+    %dlmwrite(filename_ir, ir_adj_mtx, 'delimiter','\t', 'precision', 1);
+    
+    jr_adj_mtx = randmio_und_connected(adj_mtx , round(iter/10));
+    filename_jr =strcat('A_aal_jr_ADJ_thr_0.', num2str(i), '.dat');
+    dlmwrite(filename_jr, jr_adj_mtx, 'delimiter','\t', 'precision', 1);
+
+    
+    %kr_adj_mtx = randomize_graph_partial(----------adj_mtx , )
+    
+    %lr_adj_mtx = randomizer_bin_und(......)
+    
+        
 end
