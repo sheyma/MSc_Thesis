19b39925d189b2bd8dd1e28c47157edea676ce11
Şeyma Bayrak <seymaba@gmail.com>
2014-06-27 18:20:06 +0200

    try out partial randomization

--- scripts/export_randomized.m
+++ scripts/export_randomized.m
@@ -3,6 +3,7 @@
 iter = 1000;
 % partly define the name of adjacency matrices
 name = 'A_aal_0_ADJ_thr_0.';
+name_struc = 'acp_w_0_ADJ_thr_0.';
 for i = 48:66  % has to be chosen manually
     
     input = strcat(name, num2str(i), '.dat' );
@@ -18,13 +19,20 @@ for i = 48:66  % has to be chosen manually
 %     filename_jr =strcat('A_aal_jr_ADJ_thr_0.', num2str(i), '.dat');
 %     dlmwrite(filename_jr, jr_adj_mtx, 'delimiter','\t', 'precision', 1);
 
-    for alpha = 0.1: 0.1 : 1;
-        a = num2str(alpha);
-        kr_adj_mtx = randomizer_bin_und(adj_mtx, alpha );
-        filename_kr = strcat('A_aal_kr_ADJ_thr_0.', num2str(i), '_alpha_', a, '.dat');
-        dlmwrite(filename_kr, kr_adj_mtx, 'delimiter','\t', 'precision', 1);
-    end    
+%     for alpha = 0.1: 0.1 : 1;
+%         a = num2str(alpha);
+%         kr_adj_mtx = randomizer_bin_und(adj_mtx, alpha );
+%         filename_kr = strcat('A_aal_kr_ADJ_thr_0.', num2str(i), '_alpha_', a, '.dat');
+%         dlmwrite(filename_kr, kr_adj_mtx, 'delimiter','\t', 'precision', 1);
+%     end
+%     
     %lr_adj_mtx = randomizer_bin_und(......)
     
+    swap = 500;
+    input_stru = strcat(name, num2str(i), '.dat' )
+    str_mtx = load(input_stru);
+    lr_adj_mtx = randomize_graph_partial_und(adj_mtx , str_mtx , swap);
+    filename_lr = strcat('A_aal_lr_ADJ_thr_0.', num2str(i), '.dat')
+    dlmwrite(filename_lr, lr_adj_mtx, 'delimiter','\t', 'precision', 1);
         
 end
