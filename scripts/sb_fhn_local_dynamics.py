4bdae9521cdd9992ae4ff924e2e53bac70775ee8
Şeyma Bayrak <seymaba@gmail.com>
2014-07-01 14:44:12 +0200

    FHN local dynamics (without coupling)

--- scripts/sb_fhn_local_dynamics.py
+++ scripts/sb_fhn_local_dynamics.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python2.7 
-
 # -*- coding: utf-8 -*-
+
+# this script aims to visualize local dynamics with FitzHugh Nagumo 
+# model with parameters chosen from literature
+
 
 from __future__ import division
 import numpy as np
@@ -25,14 +28,15 @@ from pylab import *
 #yy = np.linspace(-y_limit, y_limit, 10000)
 #xx = -params['a'] * np.ones(len(yy))
 	 
-# GHOS08 local dynamics	 
+# GHOS08 local dynamics	
+ 
 eqns = {r'x' : '(y + gamma*x - pow(x,3)/3.0) * TAU ',
 	r'y' : '-(x - alpha + b*y  ) / TAU' }
 
-params = {'gamma' : 0.9, #0.9
-	  'alpha' : 0.89, #1.9
-	  'TAU' : 4,
-	  'b' : 0.1
+params = {'gamma' : 1.0, #0.9
+	  'alpha' : 0.85, #1.9
+	  'TAU' : 1.25,
+	  'b' : 0.2
 	  }
 
 	  
@@ -57,7 +61,7 @@ Y_int = nullcl_01(X_int)
 
 dde = dde23(eqns=eqns,params=params)
 
-tfinal =50
+tfinal =100
 
 dde.set_sim_params(tfinal)
 
@@ -84,22 +88,23 @@ print "Sampling time interval : " , dt
 print "intersection of nullclines (x_0, y_0): ", X_int, Y_int
 
 fig = pl.figure(num=None, figsize=(13.5, 6), dpi=100, facecolor='w', edgecolor='k')
-fig.suptitle('[GHO08] Local Dynamics :  '+r'$\alpha$ = ' +str(params['alpha'])+
-	      r'  $\gamma$ = '+str(params['gamma']) + ' $b$ = '+ 
+fig.suptitle('FHN - Local Dynamics :  '+r'$\alpha$ = ' +str(params['alpha'])+
+	      r'  $  \gamma$ = '+str(params['gamma']) + ' $   b$ = '+ 
 	      str(params['b']) + r'  $\tau$ = '+str(params['TAU']),
 	      fontsize=14, fontweight='bold')
 
 pl.subplot(121, xlabel='t', ylabel='x(t) , y(t)')
 pl.plot(t, x, 'r', label='$x(t)$')
 pl.plot(t, y, 'b', label='$y(t)$')
-#pl.plot(sol_adap['t'],sol_adap['x'],'.r',linewidth=0.2)
-#pl.plot(sol_adap['t'],sol_adap['y'],'.b',linewidth=0.2)
+pl.plot(sol_adap['t'],sol_adap['x'],'.r',linewidth=0.2)
+pl.plot(sol_adap['t'],sol_adap['y'],'.b',linewidth=0.2)
 pl.axis([0, tfinal, -3, 3])
 lg = legend()
 lg.draw_frame(False)
 
 pl.subplot(122, xlabel='x', ylabel='y')
 pl.plot(x, y, 'r', label='$x,y$')
+pl.plot(x[0], y[0], '.r')
 #pl.plot(x_, y_ , 'b',label='$x_{nullcline}$')
 #pl.plot(xx,yy,'k',label='$y_{nullcline}$')
 #pl.plot(-params['a'],(-params['a']+pow(params['a'],3)/3),'ok')
@@ -107,9 +112,10 @@ pl.plot(x, y, 'r', label='$x,y$')
 pl.plot(x_range,nullcl_01(x_range),'b', label='$x_{nullcline}$')
 pl.plot(x_range,nullcl_02(x_range),'k',label='$y_{nullcline}$')
 pl.plot(X_int,Y_int, 'ok', linewidth=3)
-pl.axis([-2.3, 2.3, -1.5, 1.5])
+pl.axis([-2.3, 2.3, -2.5, 2.5])
 lg = legend()
 lg.draw_frame(False)
-pl.savefig("GHO08_local_dynamics.eps",format="eps")
+#pl.savefig("FHN_local_dynamics.eps",format="eps")
+#pl.savefig("GHO08_local_dynamics.eps",format="eps")
 #pl.savefig("PAN12_local_dynamics.eps",format="eps")
 pl.show()
