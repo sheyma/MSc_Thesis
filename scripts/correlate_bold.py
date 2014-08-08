23e6064039f8b5e57222590188e44cac3230f74a
Şeyma Bayrak <seymaba@gmail.com>
2014-08-08 14:28:59 +0200

    remove image plot part

--- scripts/correlate_bold.py
+++ scripts/correlate_bold.py
@@ -6,31 +6,37 @@ import numpy as np
 import sys 
 import math
 
+""" 
+	input  : job output from "calcBOLD.py" , m rows, n columns
+	
+	intermediate process : loading input, getting correlation 
+	coefficients' matrix, exporting it 
+	
+	output : correlation matrix of the input, n rows, n rows
+"""
 
+# load input data as numpy matrix
+def load_matrix(file):
+	print "reading data ..."
+	A  = np.loadtxt(file, unpack=False)
+	print "shape of input matrix : " , np.shape(A)
+	return A
 
-
-def correl(input_name , bold_input):
-	# correlation coefficient among the columns of bold_input calculated
+# correlation coefficients among the columns of a given matrix
+def correl_matrix(matrix , matrix_name):
 	# numpy array must be transposed to get the right corrcoef
-	
-	transpose_input = np.transpose(bold_input)
-	correl_matrix   = np.corrcoef(transpose_input)
-	
-	#file_name       = str(input_name[:-4] + '_corrcoeff.dat') 	
-	#np.savetxt(file_name, correl_matrix, '%.10f',delimiter='\t')
-	return correl_matrix
+	tr_matrix = np.transpose(matrix)
+	cr_matrix = np.corrcoef(tr_matrix)
+	file_name = str(matrix_name[:-4] + '_corr.dat')
+	np.savetxt(file_name, cr_matrix, '%.6f',delimiter='\t')
+	return cr_matrix
 
-def image(bold_input, simfile):
-	
-	# plots simulated functional connectivity
-	pl.figure(4)
-	N_col = np.shape(bold_input)[1]
-	extend = (0.5 , N_col+0.5 , N_col+0.5, 0.5 )	
-	pl.imshow(bold_input, interpolation='nearest', extent=extend)
-	pl.colorbar()
-	
-	image_name = simfile[0:-4] + '_CORR.eps'	
-	#pl.savefig(image_name, format="eps")
-	#pl.show()
-	return  
-	
+# user defined input name
+if __name__ == '__main__':
+	try:
+		input_name = sys.argv[1]
+	except:
+		sys.exit(1)
+		
+data_matrix		=		load_matrix(input_name)		
+corr_matrix		=		correl_matrix(data_matrix , input_name)
