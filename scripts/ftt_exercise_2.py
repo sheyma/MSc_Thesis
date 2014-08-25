85bf5ea7cac3aa9094d2ea5e291dfa8be1673784
Şeyma Bayrak <seymaba@gmail.com>
2014-08-25 15:36:19 +0200

    add script description to the top, import Bs and As from MATLAB output, fix figure axis the same for a better comparison

--- scripts/ftt_exercise_2.py
+++ scripts/ftt_exercise_2.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
+""" 
+	---generates sin wave of different frequencies, applies a 
+	low-pass filter to the sin wave, visulaizes it on time and frequency
+	domains (by fast fourier transform, fft). 
+	
+	---imports signal (e.g. bold signal), applies a low pass filter to 
+	the signal, visualizes it on time and frequency domain (fft)
+
+	the overall purpose is to compare butter and filtfilt functions of
+	PYTHON and MATLAB, therefore some data files built by corresponding
+	MATLAB functions are necessary to be imported and plotted!!!
+"""
 
 
 import numpy as np
@@ -29,9 +41,6 @@ x = np.sin(2*math.pi*v1*t) + np.sin(2*math.pi*v2*t)
 noise = 0
 X     = x+noise
 
-#np.savetxt('sinwave.dat',X,fmt='%.6f',delimiter='\t')
-#np.savetxt('sinwave_time.dat',t,fmt='%.6f',delimiter='\t')
-
 N     =  len(x)
 N_pow =  int(pow(2, math.ceil(math.log(N)/math.log(2))))
 
@@ -40,36 +49,24 @@ Xfft  = 2*abs(Xfft[0:N_pow /2 +1])
 
 fre   = float(Fs)/2 * np.linspace(0,1, N_pow/2 + 1);
 
-#np.savetxt('sinwave_fft.dat',Xfft, fmt='%.6f',delimiter='\t')
-#np.savetxt('sinwave_fre.dat',fre, fmt='%.6f',delimiter='\t')
-
-
 Wn    = 0.01   #Hz , cut-off freq 
-[Bs,As] = butter(5, Wn, btype='low',analog=False, output='ba');
+[BS,AS] = butter(5, Wn, btype='low',analog=False, output='ba');
 
-X_filt     = filtfilt(Bs,As,X)
-
-#np.savetxt('sinwave_filt.dat',X_filt, fmt='%.6f',delimiter='\t')
-
+X_filt     = filtfilt(BS,AS,X)
 
 X_filt_fft = np.fft.fft(X_filt, n=N_pow) /float(N)
 X_filt_fft = 2*abs(X_filt_fft[0:N_pow /2 +1])  
-
-#np.savetxt('sinwave_filt_fre.dat',X_filt_fft, fmt='%.6f',delimiter='\t')
-
 
 f_c       = 0.25		
 dtt       = 0.001	# (here 1 millisecond)
 f_s       = 1/dtt	# Sampling frequency (Hz)
 f_N       = f_s/2	# Nyquist frequency (Hz)
 
+# importing bold signal (calcBOLD output just before filtering)
 y     = np.loadtxt('bold_signal_matlab.dat')
 y     = y[:,0]
 
 t_y   = np.arange(0, len(y), 1)
-
-#np.savetxt('bold_y.dat',y, fmt='%.6f',delimiter='\t')
-#np.savetxt('bold_yt.dat',t_y, fmt='%.6f',delimiter='\t')
 
 m     = len(y);
 m_pow = int(pow(2, math.ceil(math.log(m)/math.log(2))))
@@ -78,12 +75,9 @@ yfft  = np.fft.fft(y , m_pow) /float(m)
 yfft  = 2*abs(yfft[0:m_pow /2 +1])  
 freq  = float(f_s)/2 * np.linspace(0,1, m_pow/2 + 1);
 
-#np.savetxt('bold_y_fft.dat',yfft, fmt='%.6f',delimiter='\t')
-#np.savetxt('bold_yfre.dat',freq, fmt='%.6f',delimiter='\t')
-
-
-
+# python's butter function generates different Bs and As
 [Bs,As] = butter(5, f_c/f_N, btype='low',analog=False, output='ba')
+# import Bs and As from MATLAB's butter function for precision
 Bs = np.loadtxt('Bs_matlab.dat')
 As = np.loadtxt('As_matlab.dat')
 
@@ -91,14 +85,10 @@ y_filt  = filtfilt(Bs,As,y)
 y_filt_fft  = np.fft.fft(y_filt , m_pow) /float(m)
 y_filt_fft  = 2*abs(y_filt_fft[0:m_pow /2 +1])  
 
-
-#np.savetxt('bold_y_filt.dat',y_filt, fmt='%.6f',delimiter='\t')
-#np.savetxt('bold_y_filt_fft.dat',y_filt_fft, fmt='%.6f',delimiter='\t')
-
-
+### import corresponding matlab data to plot and compare
 t_ma   = np.loadtxt('t_matlab.dat', unpack=True)
 X_ma   = np.loadtxt('sine_matlab.dat', unpack=True)	
-fre_ma = np.loadtxt('freq_matlab.dat', unpack=True)
+fre_ma = np.loadtxt('sin_fre_matlab.dat', unpack=True)
 Xfft_ma= np.loadtxt('sine_fft_matlab.dat', unpack=True)
 
 X_filt_ma     = np.loadtxt('X_filt_matlab.dat', unpack=True)
@@ -111,21 +101,6 @@ yfft_ma		  = np.loadtxt('yfft_matlab.dat', unpack=True)
 
 y_filt_ma	  = np.loadtxt('y_filt_matlab.dat', unpack=True)
 y_filt_fft_ma = np.loadtxt('y_filt_fft_matlab.dat', unpack=True)
-
-pl.figure(4)
-pl.subplot(1,2,1)
-pl.plot(t_y/1000, y_filt)
-pl.plot(t_y_ma/float(1000), y_filt_ma, 'r')
-pl.xlabel('time [s]')
-pl.ylabel('filtered bold signal (t)')
-#%axis([0, t(end), 4.9 , 5.4])
-pl.subplot(1,2,2)
-pl.plot(freq[0:50] , y_filt_fft[0:50])
-pl.plot(freq_ma[0:50] , y_filt_fft_ma[0:50], 'r')
-pl.ylabel('|filtered bold signal (f)|')
-pl.xlabel('f [Hz]')
-pl.show()
-
 
 pl.figure(1);
 pl.subplot(121)
@@ -176,7 +151,7 @@ lg.draw_frame(False)
 pl.title('imported bold signal')
 pl.xlabel('time [s]')
 pl.ylabel('bold signal')
-#pl.axis([0, t[-1], 4 , 5.5])
+pl.axis([t_y[0]/float(1000), t_y[-1]/float(1000), 4.6 , 5.4])
 pl.subplot(1,2,2)
 pl.plot(freq[0:50], yfft[0:50])
 pl.plot(freq_ma[0:50], yfft_ma[0:50], '.r')
@@ -187,3 +162,22 @@ pl.ylabel('|bold signal (f)|')
 pl.xlabel('f [Hz]')
 #pl.show()
 
+pl.figure(4)
+pl.subplot(1,2,1)
+pl.plot(t_y/float(1000), y_filt)
+pl.plot(t_y_ma/float(1000), y_filt_ma, 'r')
+lg = pl.legend(['python', 'matlab']) 
+lg.draw_frame(False)
+pl.title('Low-pass filter, Wn= ' + str(f_c/f_N) + ' Hz')
+pl.xlabel('time [s]')
+pl.ylabel('filtered bold signal(t)')
+pl.axis([t_y[0]/float(1000), t_y[-1]/float(1000), 4.6 , 5.4])
+pl.subplot(1,2,2)
+pl.plot(freq[0:50] , y_filt_fft[0:50])
+pl.plot(freq_ma[0:50] , y_filt_fft_ma[0:50], '.r')
+lg = pl.legend(['python', 'matlab']) 
+lg.draw_frame(False)
+pl.title('fourier transform')
+pl.ylabel('|filtered bold signal (f)|')
+pl.xlabel('f [Hz]')
+pl.show()
