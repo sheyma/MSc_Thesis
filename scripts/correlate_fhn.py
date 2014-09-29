8bb14e556d372fadf4a3f5dc74bc4cf67919aa90
Şeyma Bayrak <sheymaba@gmail.com>
2014-09-29 11:45:56 +0200

    correcting the index of max. - min. elements by adding 1, and insert correlation matrix plot here

--- scripts/correlate_fhn.py
+++ scripts/correlate_fhn.py
@@ -6,15 +6,19 @@ import numpy as np
 import sys 
 import math
 import scipy.stats as sistat
+import matplotlib.pyplot as pl
 
 """ 
 	input  : job output from "fhn_time_delays.py" , m rows, n columns
 	
 	intermediate process : loading input, which has ".xz" or ".dat" 
 	extentions, deleting the first column of input, which is the time 
-	column, getting correlation coefficients 
+	column, getting correlation coefficients between columns of input
+	matrix 
 	
-	output : correlation matrix of the input, n-1 rows, n-1 rows
+	output : correlation matrix of the input [n-1 rows, n-1 columns],
+	maximum and minumum correlation coefficients in this matrix and the 
+	index of those values, plot correlation matrix colorcoded
 """
 
 params = { # Fitzhugh-Nagumo simulation parameters...
@@ -44,6 +48,7 @@ def fhn_timeseries(simfile):
 
 # correlation coefficients among the columns of a given matrix
 def correl_matrix(matrix , matrix_name):
+	print "obtaining correlation coefficients among time series..."
 	# numpy array must be transposed to get the right corrcoef
 	tr_matrix = np.transpose(matrix)
 	cr_matrix = np.corrcoef(tr_matrix)
@@ -60,11 +65,41 @@ def node_index(matrix):
 		for j in range(0,np.shape(matrix)[1]):
 			if i == j :
 				matrix[i,j] = 0
+	print "max. corr. coef. in the correlation matrix:", matrix.max()
+	print "min. corr. coef. in the correlation matrix:", matrix.min()
+
 	# index of maximum value in matrix
 	[nx , ny] = np.unravel_index(matrix.argmax() , matrix.shape)
 	# index of maximum value in matrix
 	[mx , my] = np.unravel_index(matrix.argmin() , matrix.shape)
 	return nx, ny , mx, my
+
+# plots the correlation matrix of SIMULATED signal
+# input: any output of fhn_time_delays.py or output of calcBOLD.py 
+# trick: remove 1's to 0's along the diagonals 
+def plot_corr_diag(corr_matrix, matrix_name) :	
+	N_col  = np.shape(corr_matrix)[1]
+	extend = (0.5 , N_col+0.5 , N_col+0.5, 0.5 )	
+	for i in range(0,N_col):
+		for j in range(0,N_col):
+			if i==j :
+				corr_matrix[i,j] = 0
+	pl.imshow(corr_matrix, interpolation='nearest', extent=extend)
+	cbar = pl.colorbar()
+	for t in cbar.ax.get_yticklabels():
+		t.set_fontsize(15)
+	pl.xticks(fontsize = 20)
+	pl.yticks(fontsize = 20)
+	#pl.title("FHN correlation matrix\t input_name", fontsize=20)
+	pl.xlabel('Nodes', fontsize = 20)
+	pl.ylabel('Nodes', fontsize = 20)
+	if matrix_name.endswith(".xz"):
+		image_name       = str(matrix_name[:-7] + '_FHN_CORR.eps') 	
+	else :
+		image_name       = str(matrix_name[:-4] + '_FHN_CORR.eps')
+	pl.savefig(image_name, format="eps")
+	#pl.show()
+	return
 
 # user defined input name
 if __name__ == '__main__':
@@ -80,20 +115,18 @@ if input_name.endswith(".xz"):
 	xzpipe = sp.Popen(["xzcat", input_name], stdout=sp.PIPE)
 	infile = xzpipe.stdout
 else:
-	# in non-xz case we just use the file name instead of a file object, numpy's
-	# loadtxt() can deal with this
+	# in non-xz case we just use the file name instead of a file object, 
+	# numpy's loadtxt() can deal with this
 	infile = input_name
 
 data_matrix 	    =	load_matrix(infile)
 [u_matrix , T]	    =	fhn_timeseries(data_matrix)
 corr_matrix		    =	correl_matrix(u_matrix, input_name)
+plot_corr_diag(corr_matrix, input_name )
 [i, j, k , l ]		=   node_index(corr_matrix)
 
-print "nodes ", i , " and ",j, " best correlated"
-print "nodes ", k,  " and ",l, " worst correlated"
+# nodes start from 1, not from 0, therefore add 1 to the index
+print "nodes ", i+1," and ",j+1," best correlated  : ", corr_matrix[i,j] 
+print "nodes ", k+1," and ",l+1," worst correlated : ", corr_matrix[k,l]
 
-#[R_pearson_A , p_value] = sistat.pearsonr(corr_matrix[:,i] , corr_matrix[:,j])
-#[R_pearson_B , p_value] = sistat.pearsonr(corr_matrix[:,k] , corr_matrix[:,l])
-
-#print "R_pearson of nodes ", i, "and" , j ," : ", R_pearson_A 
-#print "R_pearson of nodes ", k, "and" , l ," : ", R_pearson_B 
+pl.show()
