75f9508f53c4119f795ed0281a82fcea67e17b85
Şeyma Bayrak <sheymaba@gmail.com>
2014-10-11 11:53:49 +0200

    adding the local path of the *FHN_corr.dat files to be read

--- scripts/plot_correlation.py
+++ scripts/plot_correlation.py
@@ -104,30 +104,32 @@ if __name__ == '__main__':
 		sys.exit(1)
 
 # fMRI-BOLD input matrix load , this is a correlation matrix already
-#mtx_original		=		load_matrix(input_empiri)
+mtx_empiri		=		load_matrix(input_empiri)
 
 
 ## loading correl. mtx. of simulated bold activities (calcBOLD output)
 #name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_corrcoeff.dat'
 #name_2 = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=70.0_tmax=45000_corrcoeff.dat'
 
-
 # loading correl. mtx. of fhn time series (output of correlation_fhn.py)
 #name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
 #name_2 = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
+name = 'acp_w_0_ADJ_thr_0.26_sigma=0.1_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
 
+R_thr =  {}
 
-#R_thr =  {}
-
-#for THR in np.array([54 , 56 , 58, 60 , 62, 63, 64, 65, 66]):
-	#R_temp = []
-	#for VEL in (np.arange(150, 30-10, -10)):
-		#input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]		
-		#mtx_simuli = load_matrix(input_name)
-		#R_vel      = pearson_coef(mtx_original, mtx_simuli)
-		#R_temp     = np.append(R_temp, R_vel)
-	#R_thr[THR] 	   = np.array(R_temp)
-
+for THR in np.array([26, 32, 36, 42, 46, 52, 54, 56, 58, 60, 62, 64,
+                      66, 72, 76, 82]):
+	R_temp = []
+	for VEL in np.array([70, 110, 150]):
+		local_path = '../data/jobs_corr/'
+ 		input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]		
+		mtx_simuli = load_matrix(local_path + input_name)
+		R_vel      = pearson_coef(mtx_empiri, mtx_simuli)
+		#print THR, VEL, R_vel
+		R_temp     = np.append(R_temp, R_vel)
+	R_thr[THR] 	   = np.array(R_temp)
+		
 	#for SIG in np.arange(1.0, 0.1-0.1 , -0.1):
 		#input_name = name_2[0:18] + str(THR) + name_2[20:27] + str(SIG) + name_2[30:]		
 		#mtx_simuli = load_matrix(input_name)
@@ -178,12 +180,12 @@ b = np.array([15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3])
 #pl.show()		
 
 #------------------------------------
-mtx_empiri			= 		load_matrix(input_empiri)		
+#mtx_empiri			= 		load_matrix(input_empiri)		
 #figure				=		plot_corr(mtx_empiri , input_empiri)
-mtx_simuli			= 		load_matrix(input_simuli)
-R_pearson			= 	    pearson_coef(mtx_empiri , mtx_simuli)
-print "Pearson corr. coef. between empir.-simul. : " , R_pearson
+#mtx_simuli			= 		load_matrix(input_simuli)
+#R_pearson			= 	    pearson_coef(mtx_empiri , mtx_simuli)
+#print "Pearson corr. coef. between empir.-simul. : " , R_pearson
 #figure_name 		= 		input_simuli[0:-3] + str('eps')	
 #print figure_name
 #pl.title('A_aal, 0-V , FHN \nthr=0.53 , $\sigma$ = 0.02 , tmax=450[s] v = 7 [m/s]', fontsize=20)
-pl.show()
+#pl.show()
