b02a278e901ef91edc859f43b3585ae170d86ec7
Şeyma Bayrak <sheymaba@gmail.com>
2014-10-27 22:04:48 +0100

    correlation : adjustment for acp

--- scripts/plot_correlation.py
+++ scripts/plot_correlation.py
@@ -111,15 +111,15 @@ mtx_empiri		=		load_matrix(input_empiri)
 #name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_corrcoeff.dat'
 
 # loading correl. mtx. of fhn time series (output of correlation_fhn.py)
-name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
-#name = 'acp_w_0_ADJ_thr_0.26_sigma=0.5_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
+#name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
+name = 'acp_w_0_ADJ_thr_0.54_sigma=0.3_D=0.05_v=110.0_tmax=45000_FHN_corr.dat'
 
-#thr_array = np.array([16, 22, 26, 32, 36, 42, 46, 52, 54, 56, 58, 60, 
-#					  62, 64, 66, 72, 76, 82 ])
-thr_array = np.array([54, 56, 58, 60, 62, 63, 64, 65, 66])					                        
+thr_array = np.array([16, 22, 26, 32, 36, 42, 46, 52, 54, 56, 58, 60, 
+					  62, 64, 66, 72, 76, 82 ])
+#thr_array = np.array([54, 56, 58, 60, 62, 63, 64, 65, 66])					                        
 
-#vel_array = np.array([30, 50, 70, 90, 110, 130, 150])
-vel_array = np.array([30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150])
+vel_array = np.array([30, 50, 70, 90, 110, 130, 150])
+#vel_array = np.array([30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150])
 
 sig_array = np.array([0.1, 0.3, 0.5, 0.9])
 
@@ -130,15 +130,15 @@ for THR in thr_array :
 	
 	for VEL in vel_array :
 		local_path = '../data/jobs_corr/'
- 		input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]		
-		
+ 		input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[43:]		
+		print input_name
 		try:
 			mtx_simuli = load_matrix(local_path + input_name)
 		except :
 			R_vel      = np.nan
 		else :
 			R_vel      = pearson_coef(mtx_empiri, mtx_simuli)
-			
+							
 		R_temp     = np.append(R_temp, R_vel)
 	R_thr[THR] 	   = np.array(R_temp)
 		
