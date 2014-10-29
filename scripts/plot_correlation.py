abac1504e0a15fff7bcfd135fba599c8cccbf8ac
Şeyma Bayrak <sheymaba@gmail.com>
2014-10-29 12:20:58 +0100

    make a common thr-vel-sigma range for both acp_w and A_aal

--- scripts/plot_correlation.py
+++ scripts/plot_correlation.py
@@ -106,22 +106,18 @@ if __name__ == '__main__':
 # fMRI-BOLD input matrix load , this is a correlation matrix already
 mtx_empiri		=		load_matrix(input_empiri)
 
-
-## loading correl. mtx. of simulated bold activities (calcBOLD output)
-#name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_corrcoeff.dat'
-
 # loading correl. mtx. of fhn time series (output of correlation_fhn.py)
-#name = 'A_aal_0_ADJ_thr_0.54_sigma=0.2_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
-name = 'acp_w_0_ADJ_thr_0.54_sigma=0.3_D=0.05_v=110.0_tmax=45000_FHN_corr.dat'
+name = 'A_aal_0_ADJ_thr_0.54_sigma=0.5_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
+#name = 'acp_w_h_ADJ_thr_0.54_sigma=0.5_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
 
-thr_array = np.array([16, 22, 26, 32, 36, 42, 46, 52, 54, 56, 58, 60, 
-					  62, 64, 66, 72, 76, 82 ])
-#thr_array = np.array([54, 56, 58, 60, 62, 63, 64, 65, 66])					                        
+thr_array = np.array([54, 56, 58, 60, 62, 63, 64, 65, 66])					                        
+vel_array = np.array([40, 50, 60, 70, 80])
+sig_array = np.array([0.3, 0.4, 0.5, 0.6, 0.7])
 
-vel_array = np.array([30, 50, 70, 90, 110, 130, 150])
+#thr_array = np.array([16, 22, 26, 32, 36, 42, 46, 52, 54, 56, 58, 60, 
+#					  62, 64, 66, 72, 76, 82 ])
 #vel_array = np.array([30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150])
-
-sig_array = np.array([0.1, 0.3, 0.5, 0.9])
+#sig_array = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
 
 R_thr =  {}
 
@@ -130,31 +126,23 @@ for THR in thr_array :
 	
 	for VEL in vel_array :
 		local_path = '../data/jobs_corr/'
- 		input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[43:]		
-		print input_name
-		try:
-			mtx_simuli = load_matrix(local_path + input_name)
-		except :
-			R_vel      = np.nan
-		else :
-			R_vel      = pearson_coef(mtx_empiri, mtx_simuli)
-							
-		R_temp     = np.append(R_temp, R_vel)
-	R_thr[THR] 	   = np.array(R_temp)
-		
+		input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]	
+ 		#input_name = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[43:]		
+
 	#for SIG in sig_array :
 		#local_path = '../data/jobs_corr/'
 		#input_name = name[0:18] + str(THR) + name[20:27] + str(SIG) + name[30:]		
 	
-		#try:
-			#mtx_simuli = load_matrix(local_path + input_name)
-		#except :
-			#R_sig      = np.nan
-		#else :
-			#R_sig      = pearson_coef(mtx_empiri, mtx_simuli)
+		try:
+			mtx_simuli = load_matrix(local_path + input_name)
+		except :
+			R_val      = np.nan
+		else :
+			R_val      = pearson_coef(mtx_empiri, mtx_simuli)
 	
-		#R_temp     = np.append(R_temp, R_sig)
-	#R_thr[THR] 	   = np.array(R_temp)
+		
+		R_temp     = np.append(R_temp, R_val)
+	R_thr[THR] 	   = np.array(R_temp)
 
 
 	
@@ -172,25 +160,25 @@ fig , ax = pl.subplots()
 pl.imshow(np.transpose(datam), interpolation='nearest', cmap='jet', aspect='auto')
 cbar = pl.colorbar()
 
-## PLOT PA OVER SIGMA
-#a = thr_array
-#b = sig_array
-## title for fhn....
-#pl.title('A_aal_0...' + ' , FHN , ' + '  v = 7 [m/s] '+' T = 450 [s]',
-		 #fontsize=20)
-## title for bold...
-##pl.title('A_aal_0...' + ' , BOLD , ' + '  v = 7 [m/s]', fontsize=20)
-#pl.ylabel('$\sigma$ ', fontsize=20)
-
-# PLOT PA OVER VELOCITY
+# PLOT PA OVER SIGMA
 a = thr_array
-b = vel_array
-## title for fhn....
-#pl.title('acp_w_0_...' + ' , FHN , ' + '$\sigma$ = 0.5 '+' T = 450 [s]',
-#		 fontsize=20)
-## title for bold...
-#pl.title('A_aal_0...' + ' , BOLD , ' + '$\sigma$ = 0.2', fontsize=20)
-pl.ylabel('v [m/s]', fontsize=20)
+b = sig_array
+# title for fhn....
+pl.title('A_aal_0...' + ' , FHN , ' + '  v = 7 [m/s] '+' T = 450 [s]',
+		 fontsize=20)
+# title for bold...
+#pl.title('A_aal_0...' + ' , BOLD , ' + '  v = 7 [m/s]', fontsize=20)
+pl.ylabel('$\sigma$ ', fontsize=20)
+
+## PLOT PA OVER VELOCITY
+#a = thr_array
+#b = vel_array
+### title for fhn....
+##pl.title('acp_w_0_...' + ' , FHN , ' + '$\sigma$ = 0.5 '+' T = 450 [s]',
+##		 fontsize=20)
+### title for bold...
+##pl.title('A_aal_0...' + ' , BOLD , ' + '$\sigma$ = 0.2', fontsize=20)
+#pl.ylabel('v [m/s]', fontsize=20)
 
 pl.setp(ax , xticks=np.arange(0,len(a),1), xticklabels = a)
 pl.setp(ax , yticks=np.arange(0,len(b),1), yticklabels = b)
