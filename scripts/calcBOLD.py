4deb1635e7094ae3d1d483791011dd2c4f7453b7
Şeyma Bayrak <sheymaba@gmail.com>
2014-11-04 21:20:30 +0100

    figure closure, not exporting normalized timeseries, better name for bold signal exportet

--- scripts/calcBOLD.py
+++ scripts/calcBOLD.py
@@ -133,7 +133,6 @@ def plot_timeseries(t_start , t_range , timeseries):
 # standart normalization of timeseries (MATLAB's zscore)			
 def normalize_timeseries(timeseries):
 	N_timeseries = sta.mstats.zscore(timeseries)
-	np.savetxt('../data/tmp/bak_N_python.dat', N_timeseries,'%.6f',delimiter='\t')			
 	return N_timeseries
 			
 def calc_bold(timeseries , T, out_basename):
@@ -157,7 +156,7 @@ def calc_bold(timeseries , T, out_basename):
 		if count_nan > 0:
 			print "u_N, nu. of NaNs:", Bold_signal[key][col], count_nan
 	# exporting BOLD signal 
-	file_name = str(out_basename + '_BOLD_signal.dat')
+	file_name = str(out_basename + '_Norm_BOLD_signal.dat')
 	print file_name
 	f = open(file_name,'w')	
 	for row in range( 0, len(Bold_signal[0]) ):
@@ -293,7 +292,7 @@ out_basename    = sb.get_dat_basename(input_name)
 
 print "T : " , T, " [seconds]"
 
-N_timeseries   = normalize_timeseries(timeseries)
+Norm_timeseries   = normalize_timeseries(timeseries)
 
 #pl.figure(1)
 #plot_timeseries(t_start , t_range , timeseries)
@@ -302,16 +301,16 @@ N_timeseries   = normalize_timeseries(timeseries)
 #pl.figure(3)
 #plot_timeseries(t_start , t_range , N_timeseries)
 
-bold_signal     =   calc_bold(timeseries, T, out_basename)
+#bold_signal     =   calc_bold(timeseries, T, out_basename)
 
-bold_signal_N   =   calc_bold(N_timeseries, T, out_basename)
+Norm_bold_signal   =   calc_bold(Norm_timeseries, T, out_basename)
 
-pl.figure(5)
+#pl.figure(5)
 #pl.plot(bold_signal[1])
-plot_bold_signal(T , bold_signal)
-pl.figure(6)
+#plot_bold_signal(T , bold_signal)
+#pl.figure(6)
 #pl.plot(bold_signal_N[1])
-plot_bold_signal(T , bold_signal_N)
+#plot_bold_signal(T , Norm_bold_signal)
 
 
 pl.show()
