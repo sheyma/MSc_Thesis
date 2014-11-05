98e3052ca0bf51ea18939e2fcc5d74d0a95edd34
Şeyma Bayrak <sheymaba@gmail.com>
2014-11-05 18:09:14 +0100

    compare only method 0 with a,d,g,...

--- scripts/plot_histo.py
+++ scripts/plot_histo.py
@@ -25,6 +25,17 @@ def load_matrix(file):
 
 
 def corr_histo(corr_matrix, simfile):
+		
+	#corr_flat = []
+	## removing the diagonal elements in both matrices to get
+	## more reasonable Pearson correlation coef. between them
+	#for i in range(0, np.shape(corr_matrix)[0]):
+		#for j in range(0, np.shape(corr_matrix)[1]):
+			#if (i == j) :
+
+				#tmp_a  = np.append(corr_matrix[i, 0:j], corr_matrix[i, j+1:])
+				#corr_flat  = np.append(corr_flat, tmp_a)
+
 	# merge 2D corr_matrix into 1D numpy array by flatten
 	corr_flat = np.ndarray.flatten(corr_matrix) 
 	corr_max  = 1.0
@@ -115,6 +126,7 @@ mtx_empiri = load_matrix(input_empiri)
 HistA      = corr_histo(mtx_empiri, input_empiri)
 
 # loading correl. mtx. of fhn time series (output of correlation_fhn.py)
+#name = 'A_aal_a_ADJ_thr_0.54_sigma=0.5_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
 name = 'A_aal_0_ADJ_thr_0.54_sigma=0.5_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
 #name = 'acp_w_0_ADJ_thr_0.54_sigma=0.5_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
 
@@ -127,80 +139,82 @@ sig_array = np.array([0.3, 0.4, 0.5, 0.6, 0.7])
 R_thr =  {}
 
 
-for THR in thr_array :
-	R_temp = []
+#for THR in thr_array :
+	#R_temp = []
 	
-	for VEL in vel_array :
-		local_path   = '../data/jobs_corr/'
- 		input_simuli = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]		
-		print str(THR)	
-		print input_simuli
-		try:
-			mtx_simuli = load_matrix(local_path + input_simuli)
-			HistB      = corr_histo(mtx_simuli, input_simuli)
-		except :
-			R_vel      = np.nan
-		else :
-			#R_vel      = intersec_hists(HistA, HistB)
+	#for VEL in vel_array :
+		#local_path   = '../data/jobs_corr/'
+ 		#input_simuli = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]		
+		#print str(THR)	
+		#print input_simuli
+		#try:
+			#mtx_simuli = load_matrix(local_path + input_simuli)
+			#HistB      = corr_histo(mtx_simuli, input_simuli)
+		#except :
+			#R_vel      = np.nan
+		#else :
+			##R_vel      = intersec_hists(HistA, HistB)
 			#R_vel      = chi2_hists(HistA, HistB)
-			R_vel      = bhatta_hists(HistA, HistB)
-			#R_vel = correl_hists(HistA, HistB)
+			##R_vel      = bhatta_hists(HistA, HistB)
+			##R_vel = correl_hists(HistA, HistB)
 			
-		R_temp     = np.append(R_temp, R_vel)
-	R_thr[THR] 	   = np.array(R_temp)	
+		#R_temp     = np.append(R_temp, R_vel)
+	#R_thr[THR] 	   = np.array(R_temp)	
 	
-Ordered_R   = collections.OrderedDict(sorted(R_thr.items()))	
-#print "Ordered dict"
-#print Ordered_R
+#Ordered_R   = collections.OrderedDict(sorted(R_thr.items()))	
+##print "Ordered dict"
+##print Ordered_R
 
-datam 		= np.array(Ordered_R.values())
+#datam 		= np.array(Ordered_R.values())
 
 #print "check its numpy array version"
 #print datam
 
-# PLOTTING BEGINS ! 
-fig, ax = pl.subplots()
-cmap    = pl.cm.jet
-#pl.imshow(np.transpose(datam), interpolation='nearest', cmap='jet', vmin=0.0, vmax=0.48, aspect='auto')
-pl.imshow(np.transpose(datam), interpolation='nearest', cmap='jet', aspect='auto')
-cbar = pl.colorbar()
+## PLOTTING BEGINS ! 
+#fig, ax = pl.subplots()
+#cmap    = pl.cm.jet
+##pl.imshow(np.transpose(datam), interpolation='nearest', cmap='jet', vmin=0.0, vmax=0.48, aspect='auto')
+#pl.imshow((np.transpose(datam)), interpolation='nearest', cmap='jet', aspect='auto')
+#cbar = pl.colorbar()
 
-## PLOT PA OVER SIGMA
+### PLOT PA OVER SIGMA
+##a = thr_array
+##b = sig_array
+### title for fhn....
+##pl.title('A_aal_0...' + ' , FHN , ' + '  v = 7 [m/s] '+' T = 450 [s]',
+		 ##fontsize=20)
+### title for bold...
+###pl.title('A_aal_0...' + ' , BOLD , ' + '  v = 7 [m/s]', fontsize=20)
+##pl.ylabel('$\sigma$ ', fontsize=20)
+
+## PLOT PA OVER VELOCITY
 #a = thr_array
-#b = sig_array
+#b = vel_array
 ## title for fhn....
-#pl.title('A_aal_0...' + ' , FHN , ' + '  v = 7 [m/s] '+' T = 450 [s]',
-		 #fontsize=20)
-## title for bold...
-##pl.title('A_aal_0...' + ' , BOLD , ' + '  v = 7 [m/s]', fontsize=20)
-#pl.ylabel('$\sigma$ ', fontsize=20)
+##pl.title('acp_w_0_...' + ' , FHN , ' + '$\sigma$ = 0.5 '+' T = 450 [s]',
+##		 fontsize=20)
+#pl.title('A_aal_0...' + ' , FHN , correl. test, bins=100' + '$\sigma$ = 0.5', fontsize=20)
+#pl.ylabel('v [m/s]', fontsize=20)
 
-# PLOT PA OVER VELOCITY
-a = thr_array
-b = vel_array
-# title for fhn....
-#pl.title('acp_w_0_...' + ' , FHN , ' + '$\sigma$ = 0.5 '+' T = 450 [s]',
-#		 fontsize=20)
-pl.title('A_aal_0...' + ' , FHN , correl. test, bins=100' + '$\sigma$ = 0.5', fontsize=20)
-pl.ylabel('v [m/s]', fontsize=20)
+#pl.setp(ax , xticks=np.arange(0,len(a),1), xticklabels = a)
+#pl.setp(ax , yticks=np.arange(0,len(b),1), yticklabels = b)
+#pl.xlabel('thr', fontsize = 20)
+#for t in cbar.ax.get_yticklabels():
+	#t.set_fontsize(15)
+#pl.xticks(fontsize = 15)
+#pl.yticks(fontsize = 15)
+#pl.show()		
 
-pl.setp(ax , xticks=np.arange(0,len(a),1), xticklabels = a)
-pl.setp(ax , yticks=np.arange(0,len(b),1), yticklabels = b)
-pl.xlabel('thr', fontsize = 20)
-for t in cbar.ax.get_yticklabels():
-	t.set_fontsize(15)
-pl.xticks(fontsize = 15)
-pl.yticks(fontsize = 15)
-pl.show()		
+mtx_empiri			= 		load_matrix(input_empiri)		
+mtx_simuli			= 		load_matrix(input_simuli)
+pl.figure(1)
+HistA = corr_histo(mtx_empiri, input_empiri)
+pl.figure(2)
+HistB = corr_histo(mtx_simuli, input_simuli)
+print intersec_hists(HistA, HistB)
 
-#mtx_empiri			= 		load_matrix(input_empiri)		
-#mtx_simuli			= 		load_matrix(input_simuli)
-#pl.figure(1)
-#HistA = corr_histo(mtx_empiri, input_empiri)
-#pl.figure(2)
-#HistB = corr_histo(mtx_simuli, input_simuli)
-#print intersec_hists(HistA, HistB)
-#print chi2_distance(HistA, HistB)
-
-#pl.show()
+print chi2_hists(HistA, HistB)
+print bhatta_hists(HistA, HistB)
+print correl_hists(HistA, HistB)
+pl.show()
 
