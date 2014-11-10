98e98a06aef1cdb6fe36088f923af7aa48363c87
Şeyma Bayrak <sheymaba@gmail.com>
2014-11-10 10:49:54 +0100

    plot_histo : generate histogram with numpy instead of matplotlib

--- scripts/plot_histo.py
+++ scripts/plot_histo.py
@@ -42,8 +42,10 @@ def corr_histo(corr_matrix, simfile):
 	corr_min  = -1.0
 	bin_nu    = 100
 	# a normalized histogram is obtained
-	hist = pl.hist(corr_flat, bins=bin_nu, range=[corr_min, corr_max], normed =True, histtype='bar')
-	pl.title(simfile)
+	hist, bin_edges = np.histogram(corr_flat, bins=bin_nu, range=[corr_min, corr_max], normed =True)#, histtype='bar')
+	print hist
+	#hist = pl.hist(corr_flat, bins=bin_nu, range=[corr_min, corr_max], normed =True, histtype='bar')
+	#pl.title(simfile)
 	# type(hist) = <type 'tuple'> and len(hist) = 3
 	# y_axis : hist[0] , normalized hist values
 	# x_axis : hist[1] , start points of bins 
@@ -53,8 +55,8 @@ def corr_histo(corr_matrix, simfile):
 # HA and HB must be normalized histograms
 def intersec_hists(HA, HB):
 	y_axis    = 0
-	HA_norm = HA[y_axis]
-	HB_norm = HB[y_axis]
+	HA_norm = HA
+	HB_norm = HB
 	minsum  = 0
 	for i in range(0, len(HA_norm)) :
 		minsum = minsum + min(HA_norm[i], HB_norm[i])
@@ -83,8 +85,8 @@ def correl_hists(HA, HB):
 # HA and HB must be normalized histograms
 def bhatta_hists(HA, HB):
 	y_axis    = 0
-	HA_norm = HA[y_axis]
-	HB_norm = HB[y_axis]
+	HA_norm = HA
+	HB_norm = HB
 	N  = len(HA_norm)
 	HA_bar  = sum(HA_norm) / float(N)
 	HB_bar  = sum(HB_norm) / float(N)
@@ -112,71 +114,71 @@ def chi2_hists(HA, HB):
 		squsum = squsum + tmp1 / float(tmp2)
 	return squsum
 		
-if __name__ == '__main__':
-	usage = 'Usage: %s method correlation_matrix [threshold]' % sys.argv[0]
-	try:
-		input_empiri = sys.argv[1]
-		input_simuli = sys.argv[2]
-	except:
-		print usage
-		sys.exit(1)
-		
-# fMRI-BOLD input matrix load , this is a correlation matrix already
-mtx_empiri = load_matrix(input_empiri)
-HistA      = corr_histo(mtx_empiri, input_empiri)
+#if __name__ == '__main__':
+	#usage = 'Usage: %s method correlation_matrix [threshold]' % sys.argv[0]
+	#try:
+		#input_empiri = sys.argv[1]
+		#input_simuli = sys.argv[2]
+	#except:
+		#print usage
+		#sys.exit(1)
 
-# loading correl. mtx. of fhn time series (output of correlation_fhn.py)
-#name = 'A_aal_a_ADJ_thr_0.54_sigma=0.5_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
-name = 'A_aal_0_ADJ_thr_0.54_sigma=0.5_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
-#name = 'acp_w_0_ADJ_thr_0.54_sigma=0.5_D=0.05_v=70.0_tmax=45000_FHN_corr.dat'
+local_path   = '../data/jobs_corr/'		
 
+# simulations based on EMPIRICAL brain networks
+name_E = 'A_aal_0_ADJ_thr_0.54_sigma=0.5_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
+# simulations based on RANDOMIZED brain networks
+name_R = 'A_aal_a_ADJ_thr_0.54_sigma=0.5_D=0.05_v=90.0_tmax=45000_FHN_corr.dat'
 
 thr_array = np.array([ 54,  56,  58,  60,  62,  64, 66])	
-#thr_array = np.array([54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66])					                        
 vel_array = np.array([40, 50, 60, 70, 80, 90])
 sig_array = np.array([0.3, 0.4, 0.5, 0.6, 0.7])
 
 R_thr =  {}
 
-
-#for THR in thr_array :
-	#R_temp = []
+for THR in thr_array :
+	R_temp = []
 	
-	#for VEL in vel_array :
-		#local_path   = '../data/jobs_corr/'
- 		#input_simuli = name[0:18] + str(THR) + name[20:40] + str(VEL) + name[42:]		
-		#print str(THR)	
-		#print input_simuli
-		#try:
-			#mtx_simuli = load_matrix(local_path + input_simuli)
-			#HistB      = corr_histo(mtx_simuli, input_simuli)
-		#except :
-			#R_vel      = np.nan
-		#else :
-			##R_vel      = intersec_hists(HistA, HistB)
+	for VEL in vel_array :
+ 		input_empiri = name_E[0:18] + str(THR) + name_E[20:40] + str(VEL) + name_E[42:]		
+		input_simuli = name_R[0:18] + str(THR) + name_R[20:40] + str(VEL) + name_R[42:]
+		print str(THR)
+		print local_path+input_empiri	
+		print local_path+input_simuli
+
+		try:
+			mtx_empiri = load_matrix(local_path + input_empiri)
+			HistA      = corr_histo(mtx_empiri, input_empiri)
+			mtx_simuli = load_matrix(local_path + input_simuli)
+			HistB      = corr_histo(mtx_simuli, input_simuli)
+		except :
+			R_vel      = np.nan
+		else :
+			#R_vel      = intersec_hists(HistA, HistB)
 			#R_vel      = chi2_hists(HistA, HistB)
-			##R_vel      = bhatta_hists(HistA, HistB)
-			##R_vel = correl_hists(HistA, HistB)
+			R_vel      = bhatta_hists(HistA, HistB)
+			#R_vel      = correl_hists(HistA, HistB)
+		print R_vel
 			
-		#R_temp     = np.append(R_temp, R_vel)
-	#R_thr[THR] 	   = np.array(R_temp)	
+		R_temp     = np.append(R_temp, R_vel)
+	R_thr[THR] 	   = np.array(R_temp)	
 	
-#Ordered_R   = collections.OrderedDict(sorted(R_thr.items()))	
-##print "Ordered dict"
-##print Ordered_R
+Ordered_R   = collections.OrderedDict(sorted(R_thr.items()))	
+#print "Ordered dict"
+#print Ordered_R
 
-#datam 		= np.array(Ordered_R.values())
+datam 		= np.array(Ordered_R.values())
 
 #print "check its numpy array version"
 #print datam
 
 ## PLOTTING BEGINS ! 
-#fig, ax = pl.subplots()
-#cmap    = pl.cm.jet
+fig, ax = pl.subplots()
+cmap    = pl.cm.jet
 ##pl.imshow(np.transpose(datam), interpolation='nearest', cmap='jet', vmin=0.0, vmax=0.48, aspect='auto')
-#pl.imshow((np.transpose(datam)), interpolation='nearest', cmap='jet', aspect='auto')
-#cbar = pl.colorbar()
-
+pl.imshow((np.transpose(datam)), interpolation='nearest', cmap='jet', aspect='auto')
+cbar = pl.colorbar()
+pl.show()
 ### PLOT PA OVER SIGMA
 ##a = thr_array
 ##b = sig_array
@@ -205,16 +207,16 @@ R_thr =  {}
 #pl.yticks(fontsize = 15)
 #pl.show()		
 
-mtx_empiri			= 		load_matrix(input_empiri)		
-mtx_simuli			= 		load_matrix(input_simuli)
-pl.figure(1)
-HistA = corr_histo(mtx_empiri, input_empiri)
-pl.figure(2)
-HistB = corr_histo(mtx_simuli, input_simuli)
-print intersec_hists(HistA, HistB)
+#mtx_empiri			= 		load_matrix(local_path+input_empiri)		
+#mtx_simuli			= 		load_matrix(local_path+input_simuli)
+#pl.figure(1)
+#HistA = corr_histo(mtx_empiri, input_empiri)
+#pl.figure(2)
+#HistB = corr_histo(mtx_simuli, input_simuli)
+#print intersec_hists(HistA, HistB)
 
-print chi2_hists(HistA, HistB)
-print bhatta_hists(HistA, HistB)
-print correl_hists(HistA, HistB)
-pl.show()
+#print chi2_hists(HistA, HistB)
+#print "Bahttta : ", bhatta_hists(HistA, HistB)
+#print correl_hists(HistA, HistB)
+#pl.show()
 
